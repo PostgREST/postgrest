c9e92ffc6795e2ad24a364e888dde05f6d7f94ef
Joe Nelson <cred+github@begriffs.com>
2014-08-10 19:15:43 -0700

    WIP: load fixture before all tests

--- test/PgQuerySpec.hs
+++ test/PgQuerySpec.hs
@@ -7,21 +7,22 @@ import Database.HDBC.PostgreSQL
 
 loadFixture :: String -> IO ()
 loadFixture name = do
-  sql <- readFile $ "test/fixtures/" ++ name ++ ".sql"
   conn <- connectPostgreSQL "postgres://postgres:@localhost:5432/dbapi_test"
+  sql <- readFile $ "test/fixtures/" ++ name ++ ".sql"
+  runRaw conn "drop schema if exists public cascade"
+  runRaw conn "create schema public"
   runRaw conn sql
   commit conn
 
 main :: IO ()
-main = do
-  hspec spec
+main = hspec spec
 
 spec :: Spec
 spec = beforeAll (loadFixture "schema") $ do
   describe "insert" $
-    it "can insert into an empty table" $
+    it "can insert into an empty table" $ \_ ->
       head [23 ..] `shouldBe` (23 :: Int)
 
   describe "insert again" $
-    it "is true" $
+    it "is true" $ \_ ->
       True `shouldBe` True
