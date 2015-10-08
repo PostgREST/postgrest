6ba4dc461712fa3e7751fe18a8feb0472cebe49f
Ruslan Talpa <ruslan.talpa@gmail.com>
2015-10-08 11:33:42 +0300

    Fix for #302

--- src/PostgREST/PgStructure.hs
+++ src/PostgREST/PgStructure.hs
@@ -6,8 +6,9 @@
 module PostgREST.PgStructure where
 
 import           Control.Applicative
+import           Control.Monad         (join)
 import           Data.Functor.Identity
-import           Data.List             (find)
+import           Data.List             (elemIndex, find)
 import           Data.Maybe            (fromMaybe, isJust, mapMaybe)
 import           Data.Monoid
 import           Data.Text             (Text, split)
@@ -52,8 +53,8 @@ columnFromRow (s, t, n, pos, nul, typ, u, l, p, d, e) =
     parseEnum str = fromMaybe [] $ split (==',') <$> str
 
 
-relationFromRow :: (Text, Text, Text, Text, Text) -> Relation
-relationFromRow (s, t, c, ft, fc) = Relation s t c ft fc Child Nothing Nothing Nothing
+relationFromRow :: (Text, Text, [Text], Text, [Text]) -> Relation
+relationFromRow (s, t, cs, ft, fcs) = Relation s t cs ft fcs Child Nothing Nothing Nothing
 
 pkFromRow :: (Text, Text, Text) -> PrimaryKey
 pkFromRow (s, t, n) = PrimaryKey s t n
@@ -95,47 +96,65 @@ allRelations :: H.Tx P.Postgres s [Relation]
 allRelations = do
   rels <- H.listEx $ [H.stmt|
     WITH table_fk AS (
-        SELECT
-            tc.table_schema, tc.table_name, kcu.column_name,
-            ccu.table_name AS foreign_table_name,
-            ccu.column_name AS foreign_column_name
-        FROM information_schema.table_constraints AS tc
-        JOIN information_schema.key_column_usage AS kcu on tc.constraint_name = kcu.constraint_name
-        JOIN information_schema.constraint_column_usage AS ccu on ccu.constraint_name = tc.constraint_name
-        WHERE   constraint_type = 'FOREIGN KEY'
-            AND tc.table_schema NOT IN ('pg_catalog', 'information_schema')
-        ORDER BY tc.table_schema, tc.table_name, kcu.column_name
+        SELECT ns.nspname AS table_schema,
+               tab.relname AS table_name,
+               column_info.cols AS columns,
+               other.relname AS foreign_table_name,
+               column_info.refs AS foreign_columns
+        FROM pg_constraint,
+           LATERAL (SELECT array_agg(cols.attname) AS cols,
+                           array_agg(cols.attnum)  AS nums,
+                           array_agg(refs.attname) AS refs
+                      FROM unnest(conkey, confkey) AS _(col, ref),
+                           LATERAL (SELECT * FROM pg_attribute
+                                     WHERE attrelid = conrelid AND attnum = col)
+                                AS cols,
+                           LATERAL (SELECT * FROM pg_attribute
+                                     WHERE attrelid = confrelid AND attnum = ref)
+                                AS refs)
+                AS column_info,
+           LATERAL (SELECT * FROM pg_namespace
+                            WHERE pg_namespace.oid = connamespace) AS ns,
+           LATERAL (SELECT * FROM pg_class WHERE pg_class.oid = conrelid) AS tab,
+           LATERAL (SELECT * FROM pg_class WHERE pg_class.oid = confrelid) AS other
+        WHERE confrelid != 0
+        ORDER BY (conrelid, column_info.nums)
     )
+
     SELECT * FROM table_fk
     UNION
     (
         SELECT
-            vcu.table_schema, vcu.view_name AS table_name, vcu.column_name,
+            vcu.table_schema,
+            vcu.view_name AS table_name,
+            array_agg(vcu.column_name::text) AS columns,
             table_fk.foreign_table_name,
-            table_fk.foreign_column_name
+            table_fk.foreign_columns
         FROM information_schema.view_column_usage as vcu
         JOIN table_fk ON
             table_fk.table_schema = vcu.view_schema AND
             table_fk.table_name = vcu.table_name AND
-            table_fk.column_name = vcu.column_name
+            vcu.column_name = ANY (table_fk.columns)
         WHERE vcu.view_schema NOT IN ('pg_catalog', 'information_schema')
-        ORDER BY vcu.table_schema, vcu.view_name, vcu.column_name
+        AND columns = table_fk.columns
+        GROUP BY vcu.table_schema, vcu.view_name, table_fk.foreign_table_name, table_fk.foreign_columns
     )
     UNION
     (
         SELECT
             vcu.view_schema as table_schema,
             table_fk.table_name,
-            table_fk.column_name,
+            table_fk.columns,
             vcu.view_name as foreign_table_name,
-            vcu.column_name as foreign_column_name
+            array_agg(vcu.column_name::text) as foreign_columns
         FROM information_schema.view_column_usage as vcu
         JOIN table_fk ON
             table_fk.table_schema = vcu.view_schema AND
             table_fk.foreign_table_name = vcu.table_name AND
-            table_fk.foreign_column_name = vcu.column_name
+            vcu.column_name = ANY (table_fk.foreign_columns)
         WHERE vcu.view_schema NOT IN ('pg_catalog', 'information_schema')
-        ORDER BY vcu.table_schema, vcu.view_name, vcu.column_name
+            AND foreign_columns = table_fk.foreign_columns
+        GROUP BY vcu.view_schema, table_fk.table_name, vcu.view_name, table_fk.columns
     )
   |]
   let simpleRelations = foldr (addParentRelation.relationFromRow) [] rels
@@ -145,8 +164,8 @@ allRelations = do
     groupFn :: Relation -> Text
     groupFn (Relation{relSchema=s, relTable=t}) = s<>"_"<>t
     link2Relation [
-      Relation{relSchema=sc, relTable=lt, relColumn=lc1, relFTable=t, relFColumn=c},
-      Relation{                           relColumn=lc2, relFTable=ft, relFColumn=fc}
+      Relation{relSchema=sc, relTable=lt, relColumns=lc1, relFTable=t, relFColumns=c},
+      Relation{                           relColumns=lc2, relFTable=ft, relFColumns=fc}
       ] = Just $ Relation sc t c ft fc Many (Just lt) (Just lc1) (Just lc2)
     link2Relation _ = Nothing
 
@@ -196,12 +215,16 @@ allColumns rels = do
   return $ map (addFK . columnFromRow) cols
 
   where
-    addFK col = col { colFK = relToFk <$> find (lookupFn col) rels }
+    addFK col = col { colFK = fk col }
+    fk col = join $ relToFk (colName col) <$> find (lookupFn col) rels
     lookupFn :: Column -> Relation -> Bool
-    lookupFn (Column{colSchema=cs, colTable=ct, colName=cn})  (Relation{relSchema=rs, relTable=rt, relColumn=rc, relType=rty}) =
-      cs==rs && ct==rt && cn==rc && rty==Child
+    lookupFn (Column{colSchema=cs, colTable=ct, colName=cn})  (Relation{relSchema=rs, relTable=rt, relColumns=rc, relType=rty}) =
+      cs==rs && ct==rt && cn `elem` rc && rty==Child
     lookupFn _ _ = False
-    relToFk (Relation{relFTable=t, relFColumn=c}) = ForeignKey t c
+    relToFk cName (Relation{relFTable=t, relColumns=cs, relFColumns=fcs}) = ForeignKey t <$> c
+      where
+        pos = elemIndex cName cs
+        c = (fcs !!) <$> pos
 
 allPrimaryKeys :: H.Tx P.Postgres s [PrimaryKey]
 allPrimaryKeys = do
