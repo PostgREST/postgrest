f7734e6aaac76d28b5be784d202c050b5eb7136b
calebmer <calebmeredith8@gmail.com>
2015-11-19 16:53:06 -0500

    Refactor QualifiedIdentifier

--- src/PostgREST/Types.hs
+++ src/PostgREST/Types.hs
@@ -25,24 +25,29 @@ data Table = Table {
 , tableInsertable :: Bool
 } deriving (Show, Ord)
 
+tableQi :: Table -> QualifiedIdentifier
+tableQi t = QualifiedIdentifier (Just $ tableSchema t) (tableName t) Nothing
+
 data ForeignKey = ForeignKey { fkCol :: Column } deriving (Show, Eq, Ord)
 
-data Column =
-    Column {
-      colTable     :: Table
-    , colName      :: Text
-    , colPosition  :: Int
-    , colNullable  :: Bool
-    , colType      :: Text
-    , colUpdatable :: Bool
-    , colMaxLen    :: Maybe Int
-    , colPrecision :: Maybe Int
-    , colDefault   :: Maybe Text
-    , colEnum      :: [Text]
-    , colFK        :: Maybe ForeignKey
-    }
-  | Star { colTable :: Table }
-  deriving (Show, Ord)
+data Column = Column {
+  colTable     :: Table
+, colName      :: Text
+, colPosition  :: Int
+, colNullable  :: Bool
+, colType      :: Text
+, colUpdatable :: Bool
+, colMaxLen    :: Maybe Int
+, colPrecision :: Maybe Int
+, colDefault   :: Maybe Text
+, colEnum      :: [Text]
+, colFK        :: Maybe ForeignKey
+} | Star {
+  colTable :: Table
+} deriving (Show, Ord)
+
+colQi :: Column -> QualifiedIdentifier
+colQi c = QualifiedIdentifier (Just $ (tableSchema . colTable) c) ((tableName . colTable) c) (Just $ colName c)
 
 type Synonym = (Column,Column)
 
@@ -57,12 +62,6 @@ data OrderTerm = OrderTerm {
 , otNullOrder :: Maybe BS.ByteString
 } deriving (Show, Eq)
 
-data QualifiedIdentifier = QualifiedIdentifier {
-  qiSchema :: Schema
-, qiName   :: TableName
-} deriving (Show, Eq)
-
-
 data RelationType = Child | Parent | Many deriving (Show, Eq)
 data Relation = Relation {
   relTable    :: Table
@@ -75,21 +74,47 @@ data Relation = Relation {
 , relLCols2   :: Maybe [Column]
 } deriving (Show, Eq)
 
+data QualifiedIdentifier = QualifiedIdentifier {
+  qiSchema :: Maybe Schema
+, qiTable  :: TableName
+, qiColumn :: Maybe Text
+} | UnqualifiedIdentifier {
+  qiName   :: Text
+} deriving (Show, Eq, Ord)
 
-type Operator = Text
-data FValue = VText Text | VForeignKey QualifiedIdentifier ForeignKey deriving (Show, Eq)
-type FieldName = Text
+type Path = [Text]
 type JsonPath = [Text]
-type Field = (FieldName, Maybe JsonPath)
+type Operator = Text
+data FValue = VText Text | VForeignKey QualifiedIdentifier deriving (Show, Eq)
+type Field = (QualifiedIdentifier, Maybe JsonPath)
 type Cast = Text
 type NodeName = Text
 type SelectItem = (Field, Maybe Cast)
-type Path = [Text]
-data Query = Select { select::[SelectItem], from::[Text], where_::[Filter], order::Maybe [OrderTerm] }
-           | Insert { into::Text, fields::[Field], values::[[Value]] }
-           | Delete { from::[Text], where_::[Filter] }
-           | Update { into::Text, set::Map Field Value, where_::[Filter] } deriving (Show, Eq)
-data Filter = Filter {field::Field, operator::Operator, value::FValue} deriving (Show, Eq)
+
+data Query = Select {
+  select :: [SelectItem]
+, from   :: [QualifiedIdentifier]
+, where_ :: [Filter]
+, order  :: Maybe [OrderTerm]
+} | Insert {
+  into   :: QualifiedIdentifier
+, fields :: [Field]
+, values :: [[Value]]
+} | Delete {
+  from   :: [QualifiedIdentifier]
+, where_ :: [Filter]
+} | Update {
+  into   :: QualifiedIdentifier
+, set    :: Map Field Value
+, where_ :: [Filter]
+} deriving (Show, Eq)
+
+data Filter = Filter {
+  field    :: Field
+, operator :: Operator
+, value    :: FValue
+} deriving (Show, Eq)
+
 type ApiNode = (Query, (NodeName, Maybe Relation))
 type ApiRequest = Tree ApiNode
 
@@ -126,8 +151,7 @@ instance ToJSON Table where
     , "insertable" .= tableInsertable v ]
 
 instance Eq Table where
-  Table{tableSchema=s1,tableName=n1} == Table{tableSchema=s2,tableName=n2} = s1 == s2 && n1 == n2
+  t1 == t2 = tableQi t1 == tableQi t2
 
 instance Eq Column where
-  Column{colTable=t1,colName=n1} == Column{colTable=t2,colName=n2} = t1 == t2 && n1 == n2
-  _ == _ = False
+  c1 == c2 = colQi c1 == colQi c2
