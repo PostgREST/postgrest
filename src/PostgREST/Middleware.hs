4615597fbb1beeadd4d64fa5d1b9c1aca20f67b4
Joe Nelson <cred+github@begriffs.com>
2016-09-03 17:28:37 -0700

    Semantic WWW-Authenticate headers for problematic JWT
    Adapting an OAuth 2.0 convention from RFC 6750 for use with JWT

--- src/PostgREST/Middleware.hs
+++ src/PostgREST/Middleware.hs
@@ -7,16 +7,17 @@ import           Data.Aeson                    (Value (..))
 import qualified Data.HashMap.Strict           as M
 import qualified Hasql.Transaction             as H
 
-import           Network.HTTP.Types.Status     (badRequest400, unauthorized401)
-import           Network.Wai                   (Application, Response)
+import           Network.HTTP.Types.Status     (unauthorized401)
+import           Network.Wai                   (Application, Response,
+                                                responseLBS)
 import           Network.Wai.Middleware.Cors   (cors)
 import           Network.Wai.Middleware.Gzip   (def, gzip)
 import           Network.Wai.Middleware.Static (only, staticPolicy)
 
-import           PostgREST.ApiRequest          (ApiRequest(..))
+import           PostgREST.ApiRequest          (ApiRequest(..), ContentType(..),
+                                                ctToHeader)
 import           PostgREST.Auth                (claimsToSQL, JWTAttempt(..))
 import           PostgREST.Config              (AppConfig (..), corsPolicy)
-import           PostgREST.Error               (errResponse)
 
 import           Protolude                     hiding (concat, null)
 
@@ -25,14 +26,23 @@ runWithClaims :: AppConfig -> JWTAttempt ->
                  ApiRequest -> H.Transaction Response
 runWithClaims conf eClaims app req =
   case eClaims of
-    JWTExpired -> return $ errResponse unauthorized401 "JWT expired"
-    JWTInvalid -> return $ errResponse badRequest400 "JWT invalid"
+    JWTExpired -> return $ unauthed "JWT expired"
+    JWTInvalid -> return $ unauthed "JWT invalid"
     JWTClaims claims -> do
       -- role claim defaults to anon if not specified in jwt
       H.sql . mconcat . claimsToSQL $ M.union claims (M.singleton "role" anon)
       app req
   where
     anon = String . toS $ configAnonRole conf
+    unauthed message = responseLBS unauthorized401
+      [ ctToHeader CTApplicationJSON
+      , ( "WWW-Authenticate"
+        , "Bearer error=\"invalid_token\", " <>
+          "error_description=\"" <> message <> "\""
+        )
+      ]
+      (toS $ "{\"message\":\""<>message<>"\"}")
+
 
 defaultMiddle :: Application -> Application
 defaultMiddle =
