c6cd8145eb70552960072ef7b837f16ba7ea801a
Joe Nelson <cred+github@begriffs.com>
2016-10-17 22:28:18 -0700

    Set request.jwt.claim.*

--- test/fixtures/database.sql
+++ test/fixtures/database.sql
@@ -3,4 +3,4 @@ DROP ROLE IF EXISTS postgrest_test;
 CREATE USER postgrest_test createdb createrole;
 CREATE DATABASE postgrest_test OWNER postgrest_test;
 
-ALTER DATABASE postgrest_test SET postgrest.claims.id = '-1';
+ALTER DATABASE postgrest_test SET request.jwt.claim.id = '-1';
