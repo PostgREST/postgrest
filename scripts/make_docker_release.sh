27ca778f98a56304cf18c045608e1424af34b49d
John Robinson <john.holland.robinson@gmail.com>
2016-11-11 21:11:12 -0600

    Separate stack install postgrest from docker concerns

--- scripts/make_docker_release.sh
+++ scripts/make_docker_release.sh
@@ -1,8 +1,5 @@
 RELEASE_DIR=${RELEASE_DIR:-./.release}
 
-mkdir -p $RELEASE_DIR
-
-stack install --allow-different-user --local-bin-path $RELEASE_DIR
 if [[ -e ${RELEASE_DIR}/postgrest ]]; then
     echo 'Postgrest binary exists.. \n';
     docker-compose build release;
