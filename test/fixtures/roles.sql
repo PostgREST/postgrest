a4c9290334a0e1464cae21f934c723efcf5e623c
Ruslan Talpa <ruslan.talpa@gmail.com>
2016-12-09 16:41:37 +0200

    fix circle

--- test/fixtures/roles.sql
+++ test/fixtures/roles.sql
@@ -4,4 +4,4 @@ CREATE ROLE postgrest_test_anonymous;
 CREATE ROLE postgrest_test_default_role;
 CREATE ROLE postgrest_test_author;
 
-GRANT postgrest_test_anonymous, postgrest_test_default_role, postgrest_test_author TO :AUTHENTICATOR;
+GRANT postgrest_test_anonymous, postgrest_test_default_role, postgrest_test_author TO :USER;
