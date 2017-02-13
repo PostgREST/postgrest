84f68c68cb67934df75449db8ea2cce6e484836d
Joe Nelson <begriffs@users.noreply.github.com>
2017-02-12 16:32:15 -0800

    Public schema table embedding (#803)
    * Alter fixture to test objects in public schema
    
    Properly erase and restore the public schema -- requires permissions to install pgcrypto each time.
    
    * Test for fks through public schema tables
    
    * Thanks @fab1an

--- test/fixtures/database.sql
+++ test/fixtures/database.sql
@@ -1,3 +1,3 @@
 set client_min_messages to warning;
-DROP SCHEMA IF EXISTS test, private, postgrest, jwt, تست CASCADE;
+DROP SCHEMA IF EXISTS test, private, postgrest, jwt, public, تست CASCADE;
 DROP TYPE IF EXISTS jwt_token CASCADE;
