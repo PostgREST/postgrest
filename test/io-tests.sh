5bb3b8683d724a9295c9509b8e13016bf9e50bbc
Eric Bréchemier <eric@egull.co>
2017-07-17 23:43:49 +0200

    Add unit tests for Input/Output using a separate shell script (#876)
    The script starts instances of the PostgREST server in the background,
    and runs requests with curl to check that the server accepts to verify
    a JWT corresponding the configured secret.
    
    The configuration file uses the trick value "@/dev/stdin" for jwt-secret,
    ot allow reading a different secret from a relative path using the same
    configuration file.
    
    I also tried generating a dynamic configuration file but failed to
    have PostgREST read it from standard input. I also tried to configure
    relative paths in a configuration file, but failed to have PostgREST
    to read the JWT secret from such a relative path.

---
+++ test/io-tests.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+# Run unit tests for Input/Ouput of PostgREST seen as a black box
+# with test output in Test Anything Protocol format.
+#
+# References:
+#   [1] Test Anything Protocol
+#   https://testanything.org/
+#
+#   [2] TAP Specification
+#   https://testanything.org/tap-specification.html
+#
+#   [3] List of TCP and UDP port numbers
+#   https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
+#
+cd "$(dirname "$0")"
+cd io-tests
+
+# Port for Test PostgREST Server (must match config)
+pgrPort=49421 # in range 49152–65535: for private or temporary use
+
+# TAP utilities
+currentTest=1
+bailOut(){ echo "Bail out! $1"; exit 1; }
+result(){ echo "$1 $currentTest - $2"; currentTest=$(( $currentTest + 1 )); }
+ok(){ result 'ok' "$1"; }
+ko(){ result 'not ok' "$1"; }
+comment(){ echo "# $1"; }
+
+# Utilities to start/stop test PostgREST server running in the background
+pgrStart(){ stack exec -- postgrest "$1" >/dev/null & pgrPID="$!"; }
+pgrStartRead(){ stack exec -- postgrest "$1" >/dev/null < "$2" & pgrPID="$!"; }
+pgrStarted(){ kill -0 "$pgrPID" 2>/dev/null; }
+pgrStop(){ kill "$pgrPID" 2>/dev/null; }
+pgrStopAll(){ pkill -f "$(stack path --local-install-root)/bin/postgrest"; }
+
+# Utilities to send HTTP requests to the PostgREST server
+authorsStatus(){
+  curl -s -o /dev/null -w '%{http_code}' \
+    -H "Authorization: Bearer $( cat "$1" )" \
+    "http://localhost:$pgrPort/authors_only"
+}
+
+# Start and End of Unit Tests
+setUp(){ pgrStopAll; }
+cleanUp(){ pgrStopAll; }
+
+# Unit Test Templates
+readSecretFromFile(){
+  pgrStartRead ./configs/secret-from-file.config "./secrets/$1"
+  sleep 1 # leave enough time for the server to start
+  if pgrStarted
+  then
+    authorsJwt="./secrets/${1%.*}.jwt"
+    httpStatus="$( authorsStatus "$authorsJwt" )"
+    if test "$httpStatus" -eq 200
+    then
+      ok "authentication with $2 secret read from a file"
+    else
+      ko "failed to authenticate using JWT for $2 secret: $httpStatus"
+    fi
+  else
+    ko "failed to read $2 secret from a file"
+  fi
+  pgrStop
+}
+
+# PRE: curl must be available
+test -n "$(command -v curl)" || bailOut 'curl is not available'
+
+# PRE: postgres must be running
+psql -l 1>/dev/null 2>/dev/null || bailOut 'postgres is not running'
+
+setUp
+
+totalTests=7
+echo "1..$totalTests"
+
+readSecretFromFile word.noeol 'simple (no EOL)'
+readSecretFromFile word.txt 'simple'
+readSecretFromFile ascii.noeol 'ASCII (no EOL)'
+readSecretFromFile ascii.txt 'ASCII'
+readSecretFromFile utf8.noeol 'UTF-8 (no EOL)'
+readSecretFromFile utf8.txt 'UTF-8'
+readSecretFromFile binary.noeol 'binary'
+
+cleanUp
