c0fe0a5ce10c77534aaf2bd2b6896140fb1af7bf
steve-chavez <stevechavezast@gmail.com>
2019-03-05 11:35:19 -0500

    Add repo in default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,8 @@
-with import <nixpkgs> {};
+with import (builtins.fetchGit {
+  url = https://github.com/NixOS/nixpkgs-channels;
+  ref = "nixos-18.09-small";
+  rev = "95fed28ac372c61eb83c87ad97c24b0f957827bf";
+}) {};
 
 stdenv.mkDerivation {
   name = "postgrest-docs";
