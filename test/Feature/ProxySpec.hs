dfa2340775d95f66e8ea57adfdea227b80dca93d
steve-chavez <stevechavezast@gmail.com>
2019-04-01 13:42:37 -0500

    Comment OpenAPI tests that time out
    These should be run locally for now.

--- test/Feature/ProxySpec.hs
+++ test/Feature/ProxySpec.hs
@@ -1,6 +1,7 @@
 module Feature.ProxySpec where
 
-import Test.Hspec
+import Test.Hspec hiding (pendingWith)
+import Test.Hspec.Wai
 
 import SpecHelper
 
@@ -11,5 +12,6 @@ import Protolude
 spec :: SpecWith Application
 spec =
   describe "GET / with proxy" $
-    it "returns a valid openapi spec with proxy" $
+    it "returns a valid openapi spec with proxy" $ do
+      pendingWith "Test timing out frequently on CI, please run locally"
       validateOpenApiResponse [("Accept", "application/openapi+json")]
