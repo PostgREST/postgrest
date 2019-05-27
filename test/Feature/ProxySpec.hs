65fc672417933ad58d7a6c12b7db5bf042909e17
Robert Vollmert <rob@vllmrt.net>
2019-05-27 11:40:46 +0200

    Change tests to use postgres-json-schema instead of hjsonschema.
    Embeds https://github.com/gavinwahl/postgres-json-schema.

--- test/Feature/ProxySpec.hs
+++ test/Feature/ProxySpec.hs
@@ -3,14 +3,11 @@ module Feature.ProxySpec where
 import Network.Wai (Application)
 import Test.Hspec  hiding (pendingWith)
 
-import Test.Hspec.Wai
-
 import Protolude
 import SpecHelper
 
 spec :: SpecWith Application
 spec =
   describe "GET / with proxy" $
-    it "returns a valid openapi spec with proxy" $ do
-      pendingWith "Test timing out frequently on CI, please run locally"
+    it "returns a valid openapi spec with proxy" $
       validateOpenApiResponse [("Accept", "application/openapi+json")]
