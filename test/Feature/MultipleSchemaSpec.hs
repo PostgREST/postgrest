24801f9c4d900722ca4ec224df938fd819ded6a4
Mahmoud Kassem <mahmoud_k@mail.com>
2020-01-20 23:20:29 +0100

    add more tests

--- test/Feature/MultipleSchemaSpec.hs
+++ test/Feature/MultipleSchemaSpec.hs
@@ -1,19 +1,55 @@
 module Feature.MultipleSchemaSpec where
 
 import Network.Wai (Application)
+import Network.HTTP.Types
+
 import Test.Hspec
 import Test.Hspec.Wai
 import Test.Hspec.Wai.JSON
-import Protolude       hiding (get)
+
+import Protolude
 import SpecHelper
 
 spec :: SpecWith ((), Application)
 spec =
-  describe "multiple schemas in single instance" $
-    it "read table in default schema" $ do
-      get "/table" `shouldRespondWith`
+  describe "multiple schemas in single instance" $ do
+    it "read table in default schema(v1) if no schema is selected via header" $
+      request methodGet "/table" [] "" `shouldRespondWith`
         [json|[
           {"id":1,"value":"value1"},
           {"id":2,"value":"value2"}
         ]|]
-        { matchStatus = 200, matchHeaders = [matchContentTypeJson] }
+        {
+          matchStatus = 200
+        , matchHeaders = [matchContentTypeJson]
+        }
+
+    it "read table from default schema(v1) after explicitly passing it in the header" $
+      request methodGet "/table" [("Accept-Version", "v1")] "" `shouldRespondWith`
+        [json|[
+          {"id":1,"value":"value1"},
+          {"id":2,"value":"value2"}
+        ]|]
+        {
+          matchStatus = 200
+        , matchHeaders = [matchContentTypeJson]
+        }
+
+    it "read table in other schema(v2)" $
+      request methodGet "/table" [("Accept-Version", "v2")] "" `shouldRespondWith`
+        [json|[
+          {"id":1,"value":"value3"},
+          {"id":2,"value":"value4"}
+        ]|]
+        {
+          matchStatus = 200
+        , matchHeaders = [matchContentTypeJson]
+        }
+
+    it "read table in unkown schema" $
+      request methodGet "/table" [("Accept-Version", "unkown")] "" `shouldRespondWith`
+        ""
+        {
+          matchStatus = 404
+        , matchHeaders = []
+        }
