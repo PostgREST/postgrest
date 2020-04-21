60d1aaf00d78a00db51f58e56fa457278cce87fa
Remo <remo>
2020-04-21 10:07:28 +0200

    Add a Nix derivation for just the executable.

--- default.nix
+++ default.nix
@@ -2,9 +2,14 @@ let
   name =
     "postgrest";
 
-  # PostgREST source files, filtered based on the rules in the .gitignore files.
+  # PostgREST source files, filtered based on the rules in the .gitignore files
+  # and file extensions. We want to include as litte as possible, as the files
+  # added here will increase the space used in the Nix store and trigger the
+  # build of new Nix derivations when changed.
   src =
-    pkgs.gitignoreSource ./.;
+    pkgs.lib.sourceFilesBySuffices
+      (pkgs.gitignoreSource ./.)
+      [ ".cabal" ".hs" ".lhs" "LICENSE" ];
 
   nixpkgsVersion =
     import nix/nixpkgs-version.nix;
@@ -29,14 +34,23 @@ in
 rec {
   inherit pkgs pinnedPkgs;
 
-  postgrest =
-    # Disable running the test suite on Nix builds, as they require a database
-    # to be set up.
+  # Derivation for the PostgREST Haskell package, including the executable,
+  # libraries and documentation. We disable running the test suite on Nix
+  # builds, as they require a database to be set up.
+  postgrestWithLib =
     pkgs.haskell.lib.dontCheck drv;
 
+  # Derivation for just the PostgREST binary, where we strip all dynamic
+  # libraries and documentation, leaving only the executable.
+  postgrest =
+    pkgs.haskell.lib.justStaticExecutables postgrestWithLib;
+
+  # Environment in which PostgREST can be built with cabal, useful e.g. for
+  # defining a shell for nix-shell.
   env =
     drv.env;
 
+  # Utility for updating the pinned version of Nixpkgs.
   nixpkgsUpgrade =
     pkgs.callPackage nix/nixpkgs-upgrade.nix {};
 }
