f27f8bd75901bf17a0e7082de1c2f1724db3db3f
Remo <remo>
2020-04-28 17:21:12 +0200

    more cleanup

--- src/PostgREST/DbStructure.hs
+++ src/PostgREST/DbStructure.hs
@@ -189,14 +189,14 @@ accessibleProcs = H.Statement (toS sql) (param HE.text) decodeProcs True
 procsSqlQuery :: SqlQuery
 procsSqlQuery = [q|
   SELECT
-    pn.nspname as "proc_schema",
-    p.proname as "proc_name",
-    d.description as "proc_description",
-    pg_get_function_arguments(p.oid) as "args",
-    tn.nspname as "rettype_schema",
-    coalesce(comp.relname, t.typname) as "rettype_name",
-    p.proretset as "rettype_is_setof",
-    t.typtype as "rettype_typ",
+    pn.nspname as proc_schema,
+    p.proname as proc_name,
+    d.description as proc_description,
+    pg_get_function_arguments(p.oid) as args,
+    tn.nspname as rettype_schema,
+    coalesce(comp.relname, t.typname) as rettype_name,
+    p.proretset as rettype_is_setof,
+    t.typtype as rettype_typ,
     p.provolatile
   FROM pg_proc p
     JOIN pg_namespace pn ON pn.oid = p.pronamespace
@@ -229,7 +229,7 @@ accessibleTables =
       relname as table_name,
       d.description as table_description,
       (
-        c.relkind IN ('r', 'v', 'f')
+        c.relkind in ('r', 'v', 'f')
         and (pg_relation_is_updatable(c.oid::regclass, false) & 8) = 8
         -- The function `pg_relation_is_updateable` returns a bitmask where 8
         -- corresponds to `1 << CMD_INSERT` in the PostgreSQL source code, i.e.
@@ -384,13 +384,17 @@ allTables =
       n.nspname AS table_schema,
       c.relname AS table_name,
       NULL AS table_description,
-      c.relkind = 'r' OR (c.relkind IN ('v','f'))
-      AND (pg_relation_is_updatable(c.oid::regclass, FALSE) & 8) = 8
-      OR (EXISTS
-        ( SELECT 1
+      (
+        c.relkind IN ('r', 'v','f')
+        AND (pg_relation_is_updatable(c.oid::regclass, FALSE) & 8) = 8
+        OR EXISTS (
+          SELECT 1
           FROM pg_trigger
-          WHERE pg_trigger.tgrelid = c.oid
-          AND (pg_trigger.tgtype::integer & 69) = 69) ) AS insertable
+          WHERE
+            pg_trigger.tgrelid = c.oid
+            AND (pg_trigger.tgtype::integer & 69) = 69 
+        )
+      ) AS insertable
     FROM pg_class c
     JOIN pg_namespace n ON n.oid = c.relnamespace
     WHERE c.relkind IN ('v','r','m','f')
@@ -417,9 +421,7 @@ allColumns tabs =
         info.column_default AS default_value,
         array_to_string(enum_info.vals, ',') AS enum
     FROM (
-        /*
         -- CTE based on pg_catalog to get PRIMARY/FOREIGN key and UNIQUE columns outside api schema
-        */
         WITH key_columns AS (
              SELECT
                r.oid AS r_oid,
@@ -447,14 +449,14 @@ allColumns tabs =
         -- limit columns to the ones in the api schema or PK/FK columns
         */
         columns AS (
-            SELECT current_database()::name AS table_catalog,
+            SELECT
                 nc.nspname::name AS table_schema,
                 c.relname::name AS table_name,
                 a.attname::name AS column_name,
                 d.description AS description,
-                a.attnum::information_schema.cardinal_number AS ordinal_position,
+                a.attnum::integer AS ordinal_position,
                 pg_get_expr(ad.adbin, ad.adrelid)::text AS column_default,
-                    not (a.attnotnull OR t.typtype = 'd' AND t.typnotnull) AS is_nullable,
+                not (a.attnotnull OR t.typtype = 'd' AND t.typnotnull) AS is_nullable,
                     CASE
                         WHEN t.typtype = 'd' THEN
                         CASE
@@ -472,35 +474,38 @@ allColumns tabs =
                 information_schema._pg_char_max_length(
                     information_schema._pg_truetypid(a.*, t.*),
                     information_schema._pg_truetypmod(a.*, t.*)
-                )::information_schema.cardinal_number AS character_maximum_length,
+                )::integer AS character_maximum_length,
                 information_schema._pg_numeric_precision(
                     information_schema._pg_truetypid(a.*, t.*),
                     information_schema._pg_truetypmod(a.*, t.*)
-                )::information_schema.cardinal_number AS numeric_precision,
+                )::integer AS numeric_precision,
                 COALESCE(bt.typname, t.typname)::name AS udt_name,
                 (
-                    c.relkind = 'r'
-                    OR (c.relkind = ANY (ARRAY['v', 'f']))
+                    c.relkind in ('r', 'v', 'f')
                     AND pg_column_is_updatable(c.oid::regclass, a.attnum, false)
                 )::bool is_updatable
             FROM pg_attribute a
-               LEFT JOIN key_columns kc ON kc.conkey = a.attnum AND kc.c_oid = a.attrelid
-               LEFT JOIN pg_catalog.pg_description AS d ON d.objoid = a.attrelid and d.objsubid = a.attnum
-               LEFT JOIN pg_attrdef ad ON a.attrelid = ad.adrelid AND a.attnum = ad.adnum
-               JOIN (pg_class c
-               JOIN pg_namespace nc ON c.relnamespace = nc.oid) ON a.attrelid = c.oid
-               JOIN (pg_type t
-               JOIN pg_namespace nt ON t.typnamespace = nt.oid) ON a.atttypid = t.oid
-               LEFT JOIN (pg_type bt
-               JOIN pg_namespace nbt ON bt.typnamespace = nbt.oid) ON t.typtype = 'd' AND t.typbasetype = bt.oid
-               LEFT JOIN (pg_collation co
-               JOIN pg_namespace nco ON co.collnamespace = nco.oid) ON a.attcollation = co.oid AND (nco.nspname <> 'pg_catalog'::name OR co.collname <> 'default'::name)
+                LEFT JOIN key_columns kc
+                    ON kc.conkey = a.attnum AND kc.c_oid = a.attrelid
+                LEFT JOIN pg_catalog.pg_description AS d
+                    ON d.objoid = a.attrelid and d.objsubid = a.attnum
+                LEFT JOIN pg_attrdef ad
+                    ON a.attrelid = ad.adrelid AND a.attnum = ad.adnum
+                JOIN (pg_class c JOIN pg_namespace nc ON c.relnamespace = nc.oid)
+                    ON a.attrelid = c.oid
+                JOIN (pg_type t JOIN pg_namespace nt ON t.typnamespace = nt.oid)
+                    ON a.atttypid = t.oid
+                LEFT JOIN (pg_type bt JOIN pg_namespace nbt ON bt.typnamespace = nbt.oid)
+                    ON t.typtype = 'd' AND t.typbasetype = bt.oid
+                LEFT JOIN (pg_collation co JOIN pg_namespace nco ON co.collnamespace = nco.oid)
+                    ON a.attcollation = co.oid AND (nco.nspname <> 'pg_catalog'::name OR co.collname <> 'default'::name)
             WHERE
                 NOT pg_is_other_temp_schema(nc.oid)
                 AND a.attnum > 0
                 AND NOT a.attisdropped
-                AND (c.relkind = ANY (ARRAY['r', 'v', 'f', 'm']))
-                AND (nc.nspname = ANY ($1) OR kc.r_oid IS NOT NULL) /*--filter only columns that are FK/PK or in the api schema */
+                AND c.relkind in ('r', 'v', 'f', 'm')
+                -- Filter only columns that are FK/PK or in the api schema:
+                AND (nc.nspname = ANY ($1) OR kc.r_oid IS NOT NULL)
         )
         SELECT
             table_schema,
@@ -586,14 +591,10 @@ allPrimaryKeys tabs =
   H.Statement sql HE.noParams (decodePks tabs) True
  where
   sql = [q|
-    /*
     -- CTE to replace information_schema.table_constraints to remove owner limit
-    */
     WITH tc AS (
-        SELECT current_database()::name AS constraint_catalog,
-            nc.nspname::name AS constraint_schema,
+        SELECT
             c.conname::name AS constraint_name,
-            current_database()::name AS table_catalog,
             nr.nspname::name AS table_schema,
             r.relname::name AS table_name,
                 CASE c.contype
@@ -614,22 +615,18 @@ allPrimaryKeys tabs =
             AND r.relkind = 'r'
             AND NOT pg_is_other_temp_schema(nr.oid)
     ),
-    /*
     -- CTE to replace information_schema.key_column_usage to remove owner limit
-    */
     kc AS (
-        SELECT current_database()::name AS constraint_catalog,
-            ss.nc_nspname::name AS constraint_schema,
+        SELECT
             ss.conname::name AS constraint_name,
-            current_database()::name AS table_catalog,
             ss.nr_nspname::name AS table_schema,
             ss.relname::name AS table_name,
             a.attname::name AS column_name,
-            (ss.x).n::information_schema.cardinal_number AS ordinal_position,
-                CASE
-                    WHEN ss.contype = 'f' THEN information_schema._pg_index_position(ss.conindid, ss.confkey[(ss.x).n])
-                    ELSE NULL::integer
-                END::information_schema.cardinal_number AS position_in_unique_constraint
+            (ss.x).n::integer AS ordinal_position,
+            CASE
+                WHEN ss.contype = 'f' THEN information_schema._pg_index_position(ss.conindid, ss.confkey[(ss.x).n])
+                ELSE NULL::integer
+            END::integer AS position_in_unique_constraint
         FROM pg_attribute a,
             ( SELECT r.oid AS roid,
                 r.relname,
@@ -641,7 +638,6 @@ allPrimaryKeys tabs =
                 c.contype,
                 c.conindid,
                 c.confkey,
-                c.confrelid,
                 information_schema._pg_expandarray(c.conkey) AS x
                FROM pg_namespace nr,
                 pg_class r,
@@ -651,9 +647,14 @@ allPrimaryKeys tabs =
                 nr.oid = r.relnamespace
                 AND r.oid = c.conrelid
                 AND nc.oid = c.connamespace
-                AND (c.contype = ANY (ARRAY['p', 'u', 'f']))
-                AND r.relkind = 'r' AND NOT pg_is_other_temp_schema(nr.oid)) ss
-        WHERE ss.roid = a.attrelid AND a.attnum = (ss.x).x AND NOT a.attisdropped
+                AND c.contype in ('p', 'u', 'f')
+                AND r.relkind = 'r'
+                AND NOT pg_is_other_temp_schema(nr.oid)
+            ) ss
+        WHERE
+          ss.roid = a.attrelid
+          AND a.attnum = (ss.x).x
+          AND NOT a.attisdropped
     )
     SELECT
         kc.table_schema,
@@ -693,7 +694,7 @@ allSourceColumns cols pgVer =
         from pg_class c
         join pg_namespace n on n.oid = c.relnamespace
         join pg_rewrite r on r.ev_class = c.oid
-        where (c.relkind in ('v', 'm')) and n.nspname = ANY ($1)
+        where c.relkind in ('v', 'm') and n.nspname = ANY ($1)
       ),
       removed_subselects as(
         select
