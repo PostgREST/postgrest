516809f2de85e787fa2a278193884a897b9be70a
steve-chavez <stevechavezast@gmail.com>
2020-05-20 14:07:35 -0500

    Add favicon.ico

--- conf.py
+++ conf.py
@@ -132,7 +132,7 @@ html_theme = 'sphinx_rtd_theme'
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = '_static/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
