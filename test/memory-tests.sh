18bae0681967b1f71d2f8acfc8fa9058dccf2893
Wolfgang Walther <walther@technowledgy.de>
2021-04-18 00:31:35 +0200

    lint: Apply shellcheck suggestions for bash scripts in test/*

--- test/memory-tests.sh
+++ test/memory-tests.sh
@@ -18,15 +18,15 @@ trap "kill 0" int term exit
 
 currentTest=1
 failedTests=0
-result(){ echo "$1 $currentTest $2"; currentTest=$(( $currentTest + 1 )); }
+result(){ echo "$1 $currentTest $2"; currentTest=$(( currentTest + 1 )); }
 ok(){ result 'ok' "- $1"; }
-ko(){ result 'not ok' "- $1"; failedTests=$(( $failedTests + 1 )); }
+ko(){ result 'not ok' "- $1"; failedTests=$(( failedTests + 1 )); }
 
 pgrStart(){ postgrest +RTS -p -h > /dev/null & pgrPID="$!"; }
 pgrStop(){ kill "$pgrPID" 2>/dev/null; }
 
 checkPgrStarted(){
-  while pgrStarted && test $(rootStatus) -ne 200
+  while pgrStarted && test "$(rootStatus)" -ne 200
   do
     sleep 1
   done
@@ -39,9 +39,9 @@ rootStatus(){
 jsonKeyTest(){
   pgrStart
   checkPgrStarted
-  factor=$(( 3*$(numfmt --from=si $1)/4 )) # 3/4 on $1 is need to maintain the specified size because of base64
+  factor=$(( 3*$(numfmt --from=si "$1")/4 )) # 3/4 on $1 is need to maintain the specified size because of base64
   payload="{\"blob\" : \"$(dd if=/dev/zero bs=$factor count=1 status=none | base64)\"}"
-  httpStatus=$(echo $payload | curl -s -H "Content-Type: application/json" --request $2 -d @- -w '%{http_code}' http://localhost:$pgrPort$3 | tr -d '"')
+  httpStatus=$(echo "$payload" | curl -s -H "Content-Type: application/json" --request "$2" -d @- -w '%{http_code}' http://localhost:"$pgrPort""$3" | tr -d '"')
   if test "$httpStatus" -ge 200 && test "$httpStatus" -lt 210
   then
     pgrStop
@@ -49,10 +49,10 @@ jsonKeyTest(){
     do
       sleep 1
     done
-    BYTES_FMT=$(cat postgrest.prof | grep -o -P '(?<=alloc =).*(?=bytes)' | tr -d ' ')
-    BYTES=$(echo $BYTES_FMT | tr -d ',')
-    MAX_BYTES=$(numfmt --from=si $4)
-    if test $BYTES -le $MAX_BYTES
+    BYTES_FMT=$(< postgrest.prof grep -o -P '(?<=alloc =).*(?=bytes)' | tr -d ' ')
+    BYTES=$(echo "$BYTES_FMT" | tr -d ',')
+    MAX_BYTES=$(numfmt --from=si "$4")
+    if test "$BYTES" -le "$MAX_BYTES"
     then
       ok "$2 $3: with a json key of $1 the memory usage($BYTES_FMT bytes) is less than $4"
     else
@@ -69,14 +69,14 @@ postJsonArrayTest(){
   checkPgrStarted
   arr=()
   arr+=('[')
-  for i in $(seq 1 $(expr $1 - 1))
+  for i in $(seq 1 $(("$1" - 1)))
   do
     arr+=("{\"id\": $i, \"body\": \"xxxxxxx\"},")
   done
   arr+=("{\"id\": $1, \"body\": \"xxxxxxx\"}")
   arr+=(']')
-  payload=$(echo ${arr[*]})
-  httpStatus=$(echo $payload | curl -s -H "Content-Type: application/json" -d @- -w '%{http_code}' http://localhost:$pgrPort$2 | tr -d '"')
+  payload="${arr[*]}"
+  httpStatus=$(echo "$payload" | curl -s -H "Content-Type: application/json" -d @- -w '%{http_code}' http://localhost:"$pgrPort""$2" | tr -d '"')
   if test "$httpStatus" -ge 200 && test "$httpStatus" -lt 210
   then
     pgrStop
@@ -84,11 +84,11 @@ postJsonArrayTest(){
     do
       sleep 1
     done
-    BYTES_FMT=$(cat postgrest.prof | grep -o -P '(?<=alloc =).*(?=bytes)' | tr -d ' ')
-    BYTES=$(echo $BYTES_FMT | tr -d ',')
-    MAX_BYTES=$(numfmt --from=si $3)
-    PAYLOAD_SIZE=$(echo $payload | wc -c | numfmt --to=si)
-    if test $BYTES -le $MAX_BYTES
+    BYTES_FMT=$(< postgrest.prof grep -o -P '(?<=alloc =).*(?=bytes)' | tr -d ' ')
+    BYTES=$(echo "$BYTES_FMT" | tr -d ',')
+    MAX_BYTES=$(numfmt --from=si "$3")
+    PAYLOAD_SIZE=$(echo "$payload" | wc -c | numfmt --to=si)
+    if test "$BYTES" -le "$MAX_BYTES"
     then
       ok "POST $2: with a json payload of $PAYLOAD_SIZE that has $1 array values the memory usage($BYTES_FMT bytes) is less than $3"
     else
