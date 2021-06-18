PostgREST/postgrest

commit 343e41c51d6cad33383043abb4ef15dc54a32245

Location header improvements(#1475)
* Create location header only on primary key columns, closes https://github.com/PostgREST/postgrest/issues/1461

* Return location header when pk columns not selected, fixes https://github.com/PostgREST/postgrest/issues/1162

test/Feature/InsertSpec.hs
@@ -104,6 +104,25 @@ spec actualPgVersion = do
                            , "Content-Range" <:> "*/*" ]
           }
 
+      it "should not throw and return location header when selecting without PK" $
+        request methodPost "/projects?select=name,client_id" [("Prefer", "return=representation")]
+          [str|{"id":10,"name":"New Project","client_id":2}|] `shouldRespondWith`
+          [str|[{"name":"New Project","client_id":2}]|]
+          { matchStatus  = 201
+          , matchHeaders = [ matchContentTypeJson
+                           , "Location" <:> "/projects?id=eq.10"
+                           , "Content-Range" <:> "*/*" ]
+          }
+
+    context "requesting no representation" $
+      it "should not throw and return location header when selecting without PK" $
+        request methodPost "/projects?select=name,client_id" []
+          [str|{"id":11,"name":"New Project","client_id":2}|] `shouldRespondWith` ""
+          { matchStatus  = 201
+          , matchHeaders = [ "Location" <:> "/projects?id=eq.11"
+                           , "Content-Range" <:> "*/*" ]
+          }
+
     context "from an html form" $
       it "accepts disparate json types" $ do
         p <- request methodPost "/menagerie"
@@ -139,11 +158,11 @@ spec actualPgVersion = do
           }
 
       context "into a table with no pk" $ do
-        it "succeeds with 201 and a link including all fields" $ do
+        it "succeeds with 201 but no location header" $ do
           p <- post "/no_pk" [json| { "a":"foo", "b":"bar" } |]
           liftIO $ do
             simpleBody p `shouldBe` ""
-            simpleHeaders p `shouldSatisfy` matchHeader hLocation "/no_pk\\?a=eq.foo&b=eq.bar"
+            lookup hLocation (simpleHeaders p) `shouldBe` Nothing
             simpleStatus p `shouldBe` created201
 
         it "returns full details of inserted record if asked" $ do
@@ -152,7 +171,7 @@ spec actualPgVersion = do
                        [json| { "a":"bar", "b":"baz" } |]
           liftIO $ do
             simpleBody p `shouldBe` [json| [{ "a":"bar", "b":"baz" }] |]
-            simpleHeaders p `shouldSatisfy` matchHeader hLocation "/no_pk\\?a=eq.bar&b=eq.baz"
+            lookup hLocation (simpleHeaders p) `shouldBe` Nothing
             simpleStatus p `shouldBe` created201
 
         it "returns empty array when no items inserted, and return=rep" $ do
@@ -169,7 +188,7 @@ spec actualPgVersion = do
                        [json| { "a":null, "b":"foo" } |]
           liftIO $ do
             simpleBody p `shouldBe` [json| [{ "a":null, "b":"foo" }] |]
-            simpleHeaders p `shouldSatisfy` matchHeader hLocation "/no_pk\\?a=is.null&b=eq.foo"
+            lookup hLocation (simpleHeaders p) `shouldBe` Nothing
             simpleStatus p `shouldBe` created201
 
     context "with compound pk supplied" $
@@ -238,24 +257,20 @@ spec actualPgVersion = do
     context "jsonb" $ do
       it "serializes nested object" $ do
         let inserted = [json| { "data": { "foo":"bar" } } |]
-            location = "/json?data=eq.%7B%22foo%22%3A%22bar%22%7D"
         request methodPost "/json"
                      [("Prefer", "return=representation")]
                      inserted
           `shouldRespondWith` [str|[{"data":{"foo":"bar"}}]|]
           { matchStatus  = 201
-          , matchHeaders = ["Location" <:> location]
           }
 
       it "serializes nested array" $ do
         let inserted = [json| { "data": [1,2,3] } |]
-            location = "/json?data=eq.%5B1%2C2%2C3%5D"
         request methodPost "/json"
                      [("Prefer", "return=representation")]
                      inserted
           `shouldRespondWith` [str|[{"data":[1,2,3]}]|]
           { matchStatus  = 201
-          , matchHeaders = ["Location" <:> location]
           }
 
     context "empty objects" $ do
@@ -358,8 +373,7 @@ spec actualPgVersion = do
                      "a,b\nbar,baz"
           `shouldRespondWith` "a,b\nbar,baz"
           { matchStatus  = 201
-          , matchHeaders = ["Content-Type" <:> "text/csv; charset=utf-8",
-                            "Location" <:> "/no_pk?a=eq.bar&b=eq.baz"]
+          , matchHeaders = ["Content-Type" <:> "text/csv; charset=utf-8"]
           }
 
       it "can post nulls" $
@@ -368,8 +382,7 @@ spec actualPgVersion = do
                      "a,b\nNULL,foo"
           `shouldRespondWith` "a,b\n,foo"
           { matchStatus  = 201
-          , matchHeaders = ["Content-Type" <:> "text/csv; charset=utf-8",
-                            "Location" <:> "/no_pk?a=is.null&b=eq.foo"]
+          , matchHeaders = ["Content-Type" <:> "text/csv; charset=utf-8"]
           }
 
       it "only returns the requested column header with its associated data" $
@@ -393,8 +406,8 @@ spec actualPgVersion = do
 
     context "with unicode values" $
       it "succeeds and returns usable location header" $ do
-        let payload = [json| { "a":"圍棋", "b":"￥" } |]
-        p <- request methodPost "/no_pk"
+        let payload = [json| { "k":"圍棋", "extra":"￥" } |]
+        p <- request methodPost "/simple_pk?select=extra,k"
                      [("Prefer", "return=representation")]
                      payload
         liftIO $ do
@@ -402,7 +415,7 @@ spec actualPgVersion = do
           simpleStatus p `shouldBe` created201
 
         let Just location = lookup hLocation $ simpleHeaders p
-        r <- get location
+        r <- get (location <> "&select=extra,k")
         liftIO $ simpleBody r `shouldBe` "["<>payload<>"]"
 
   describe "Patching record" $ do

src/PostgREST/App.hs
@@ -361,7 +361,7 @@ app dbStructure proc cols conf apiRequest =
           let
             readReq = readRequest s t maxRows (dbRelations dbStructure) apiRequest
             returnings :: ReadRequest -> Either Response [FieldName]
-            returnings rr = Right (returningCols rr)
+            returnings rr = Right (returningCols rr [])
           in
           (,,,) <$>
           (readRequestToQuery <$> readReq) <*>

src/PostgREST/DbRequestBuilder.hs
@@ -316,7 +316,7 @@ mutateRequest schema tName apiRequest cols pkCols readReq = mapLeft errorRespons
     returnings =
       if iPreferRepresentation apiRequest == None
         then []
-        else returningCols readReq
+        else returningCols readReq pkCols
     filters = map snd <$> mapM pRequestFilter mutateFilters
     logic = map snd <$> mapM pRequestLogicTree logicFilters
     combinedLogic = foldr addFilterToLogicForest <$> logic <*> filters
@@ -324,8 +324,11 @@ mutateRequest schema tName apiRequest cols pkCols readReq = mapLeft errorRespons
     (mutateFilters, logicFilters) = join (***) onlyRoot (iFilters apiRequest, iLogic apiRequest)
     onlyRoot = filter (not . ( "." `isInfixOf` ) . fst)
 
-returningCols :: ReadRequest -> [FieldName]
-returningCols rr@(Node _ forest) = returnings
+returningCols :: ReadRequest -> [FieldName] -> [FieldName]
+returningCols rr@(Node _ forest) pkCols
+  -- if * is part of the select, we must not add pk or fk columns manually - otherwise those would be selected and output twice
+  | "*" `elem` fldNames = ["*"]
+  | otherwise           = returnings
   where
     fldNames = fstFieldNames rr
     -- Without fkCols, when a mutateRequest to /projects?select=name,clients(name) occurs, the RETURNING SQL part would be
@@ -343,7 +346,8 @@ returningCols rr@(Node _ forest) = returnings
     -- However if the "client_id" is present, e.g. mutateRequest to /projects?select=client_id,name,clients(name)
     -- we would get `RETURNING client_id, name, client_id` and then we would produce the "column reference \"client_id\" is ambiguous"
     -- error from PostgreSQL. So we deduplicate with Set:
-    returnings = S.toList . S.fromList $ fldNames ++ (colName <$> fkCols)
+    -- We are adding the primary key columns as well to make sure, that a proper location header can always be built for INSERT/POST
+    returnings = S.toList . S.fromList $ fldNames ++ (colName <$> fkCols) ++ pkCols
 
 -- Traditional filters(e.g. id=eq.1) are added as root nodes of the LogicTree
 -- they are later concatenated with AND in the QueryBuilder

src/PostgREST/Private/QueryFragment.hs
@@ -87,10 +87,12 @@ asBinaryF fieldName = "coalesce(string_agg(_postgrest_t." <> pgFmtIdent fieldNam
 locationF :: [Text] -> SqlFragment
 locationF pKeys = [qc|(
   WITH data AS (SELECT row_to_json(_) AS row FROM {sourceCTEName} AS _ LIMIT 1)
-  SELECT array_agg(json_data.key || '=' || coalesce('eq.' || json_data.value, 'is.null'))
+  SELECT array_agg(json_data.key || '=eq.' || json_data.value)
   FROM data CROSS JOIN json_each_text(data.row) AS json_data
-  {("WHERE json_data.key IN ('" <> intercalate "','" pKeys <> "')") `emptyOnFalse` null pKeys}
+  WHERE json_data.key IN ('{fmtPKeys}')
 )|]
+  where
+    fmtPKeys = intercalate "','" pKeys
 
 fromQi :: QualifiedIdentifier -> SqlFragment
 fromQi t = (if s == "" then "" else pgFmtIdent s <> ".") <> pgFmtIdent n

commit 59e0b006a533b03a50e888f092141247268f0fae

fix: Location header with a null PK on a view (#1875)
Regression after #1475

src/PostgREST/Query/SqlFragment.hs
@@ -172,7 +172,7 @@ asBinaryF fieldName = "coalesce(string_agg(_postgrest_t." <> pgFmtIdent fieldNam
 locationF :: [Text] -> SqlFragment
 locationF pKeys = [qc|(
   WITH data AS (SELECT row_to_json(_) AS row FROM {sourceCTEName} AS _ LIMIT 1)
-  SELECT array_agg(json_data.key || '=eq.' || json_data.value)
+  SELECT array_agg(json_data.key || '=' || coalesce('eq.' || json_data.value, 'is.null'))
   FROM data CROSS JOIN json_each_text(data.row) AS json_data
   WHERE json_data.key IN ('{fmtPKeys}')
 )|]

test/Feature/InsertSpec.hs
@@ -527,7 +527,7 @@ spec actualPgVersion = do
             , matchHeaders = [ matchHeaderAbsent hLocation ]
             }
 
-    context "requesting header only representation" $
+    context "requesting header only representation" $ do
       it "returns a location header" $
         request methodPost "/compound_pk_view" [("Prefer", "return=headers-only")]
             [json|{"k1":1,"k2":"test","extra":2}|]
@@ -535,5 +535,15 @@ spec actualPgVersion = do
             ""
             { matchStatus  = 201
             , matchHeaders = [ "Location" <:> "/compound_pk_view?k1=eq.1&k2=eq.test"
+                             , "Content-Range" <:> "*/*" ]
+            }
+
+      it "should not throw and return location header when a PK is null" $
+        request methodPost "/test_null_pk_competitors_sponsors" [("Prefer", "return=headers-only")]
+            [json|{"id":1}|]
+          `shouldRespondWith`
+            ""
+            { matchStatus  = 201
+            , matchHeaders = [ "Location" <:> "/test_null_pk_competitors_sponsors?id=eq.1&sponsor_id=is.null"
                              , "Content-Range" <:> "*/*" ]
             }

test/fixtures/privileges.sql
@@ -37,6 +37,7 @@ GRANT ALL ON TABLE
     , projects
     , projects_view
     , projects_view_alt
+    , test_null_pk_competitors_sponsors
     , simple_pk
     , simple_pk2
     , tasks

test/fixtures/schema.sql
@@ -670,6 +670,28 @@ CREATE VIEW projects_view_alt AS
     projects.client_id as t_client_id
    FROM projects;
 
+CREATE TABLE sponsors (
+    id integer primary key,
+    name varchar(20) not null
+);
+
+CREATE TABLE competitors (
+    id integer primary key,
+    sponsor_id integer references sponsors(id),
+    full_name varchar(40) not null
+);
+
+CREATE VIEW test_null_pk_competitors_sponsors  AS
+SELECT c.id, s.id as sponsor_id
+FROM competitors c
+    LEFT JOIN sponsors s on c.sponsor_id = s.id;
+
+CREATE RULE test_null_pk_competitors_sponsors  AS
+    ON INSERT TO test_null_pk_competitors_sponsors DO INSTEAD
+    INSERT INTO competitors(id, full_name, sponsor_id)
+    VALUES (new.id, 'Competitor without sponsor', new.sponsor_id)
+    RETURNING id, sponsor_id;
+
 --
 -- Name: simple_pk; Type: TABLE; Schema: test; Owner: -
 --