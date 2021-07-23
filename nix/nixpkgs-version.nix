68bf17118dc5e38f0360301cc5466f20988800d2
Remo Rechkemmer <59358383+monacoremo@users.noreply.github.com>
2021-07-23 19:54:29 +0200

    Upgrade Nix dependencies including nixpkgs and static-haskell-nix (#1899)

--- nix/nixpkgs-version.nix
+++ nix/nixpkgs-version.nix
@@ -1,6 +1,6 @@
 # Pinned version of Nixpkgs, generated with postgrest-nixpkgs-upgrade.
 {
-  date = "2021-07-12";
-  rev = "c114cd459e1bac0d05e4e27c9ea43c99799cbbd1";
-  tarballHash = "1hpspw8jbzq03k8a6izxnbqww42mihqaa6c8nszw30igiaqc98i2";
+  date = "2021-07-17";
+  rev = "d00b5a5fa6fe8bdf7005abb06c46ae0245aec8b5";
+  tarballHash = "08497wbpnf3w5dalcasqzymw3fmcn8qrnbkf8rxxwwvyjdnczxdv";
 }
