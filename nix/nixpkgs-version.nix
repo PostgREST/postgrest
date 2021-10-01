8ac14447ef85236f68bf8a8b8cc8d1f3052f6224
monacoremo <59358383+monacoremo@users.noreply.github.com>
2021-10-01 16:04:16 +0200

    nix: Update nixpkgs to latest unstable version

--- nix/nixpkgs-version.nix
+++ nix/nixpkgs-version.nix
@@ -1,6 +1,6 @@
 # Pinned version of Nixpkgs, generated with postgrest-nixpkgs-upgrade.
 {
-  date = "2021-07-17";
-  rev = "d00b5a5fa6fe8bdf7005abb06c46ae0245aec8b5";
-  tarballHash = "08497wbpnf3w5dalcasqzymw3fmcn8qrnbkf8rxxwwvyjdnczxdv";
+  date = "2021-10-31";
+  rev = "9303cc044586dd40599233454f1fd79176584c0f";
+  tarballHash = "0bgdk4zw9m42i3cva55a2bp2z16kd6xchz08vl0zg8d0mr3kk0q3";
 }
