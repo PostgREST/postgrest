1287feadbb38b2267adca9a3780bb85afb9de4cc
monacoremo <59358383+monacoremo@users.noreply.github.com>
2021-11-02 19:47:17 +0100

    nix: add statix to postgrest-style

--- nix/nixpkgs-version.nix
+++ nix/nixpkgs-version.nix
@@ -1,6 +1,6 @@
 # Pinned version of Nixpkgs, generated with postgrest-nixpkgs-upgrade.
 {
-  date = "2021-10-31";
-  rev = "9303cc044586dd40599233454f1fd79176584c0f";
-  tarballHash = "0bgdk4zw9m42i3cva55a2bp2z16kd6xchz08vl0zg8d0mr3kk0q3";
+  date = "2021-11-02";
+  rev = "7053541084bf5ce2921ef307e5585d39d7ba8b3f";
+  tarballHash = "1flhh5d4zy43x6060hvzjb5hi5cmc51ivc0nwmija9n8d35kcc4x";
 }
