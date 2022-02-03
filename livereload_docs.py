71dd891d2f8309c6c58a1f6ed88d1f32bac8f203
Wolfgang Walther <walther@technowledgy.de>
2022-02-03 07:49:49 +0100

    fix postgrest-serve after move to docs/ folder
    Signed-off-by: Wolfgang Walther <walther@technowledgy.de>

--- livereload_docs.py
+++ livereload_docs.py
@@ -4,10 +4,7 @@ from subprocess import call
 ## Build docs at startup
 call(['sphinx-build', '-b', 'html', '-a', '-n', 'docs', '_build'])
 server = Server()
-server.watch('*.rst', shell('sphinx-build -b html -a -n docs _build'))
-server.watch('tutorials/*.rst', shell('sphinx-build -b html -a -n docs _build'))
-server.watch('how-tos/*.rst', shell('sphinx-build -b html -a -n docs _build'))
-server.watch('releases/*.rst', shell('sphinx-build -b html -a -n docs _build'))
+server.watch('docs/**/*.rst', shell('sphinx-build -b html -a -n docs _build'))
 # For custom port and host
 # server.serve(root='_build/', host='192.168.1.2')
 server.serve(root='_build/')
