d3b05d297685cd71a65ff36be47c04d9a234b477
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-03 13:23:17 +0800

    Delete test directory

--- test/spec/fixtures/database.sql
+++
@@ -1,6 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-ALTER DATABASE :DBNAME SET request.jwt.claim.id = '-1';
-
-set client_min_messages to warning;
-DROP SCHEMA IF EXISTS test, private, postgrest, jwt, public, تست, extensions, v1, v2 CASCADE;
-DROP TYPE IF EXISTS jwt_token CASCADE;
