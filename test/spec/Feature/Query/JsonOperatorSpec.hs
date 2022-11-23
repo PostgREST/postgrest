3103060f4b62096f67882a52ba28bcafe4c4e1a4
steve-chavez <stevechavezast@gmail.com>
2022-11-23 12:57:15 -0500

    test: move failed json parsing to doctest

--- test/spec/Feature/Query/JsonOperatorSpec.hs
+++ test/spec/Feature/Query/JsonOperatorSpec.hs
@@ -292,25 +292,11 @@ spec actualPgVersion = describe "json and jsonb operators" $ do
         [json| [{"data":[{"a": [1,2,3]}, {"b": [4,5]}]}] |]
         { matchHeaders = [matchContentTypeJson] }
 
-    it "should fail on badly formed negatives" $ do
-      get "/json_arr?select=data->>-78xy" `shouldRespondWith`
-        [json|
-          {"details": "unexpected 'x' expecting digit, \"->\", \"::\", \".\", \",\" or end of input",
-           "message": "\"failed to parse select parameter (data->>-78xy)\" (line 1, column 11)",
-           "code": "PGRST100",
-           "hint": null} |]
-        { matchStatus = 400, matchHeaders = [matchContentTypeJson] }
-      get "/json_arr?select=data->>--34" `shouldRespondWith`
-        [json|
-          {"details": "unexpected \"-\" expecting digit",
-           "message": "\"failed to parse select parameter (data->>--34)\" (line 1, column 9)",
-           "code": "PGRST100",
-           "hint": null} |]
-        { matchStatus = 400, matchHeaders = [matchContentTypeJson] }
-      get "/json_arr?select=data->>-xy-4" `shouldRespondWith`
-        [json|
-          {"details":"unexpected \"x\" expecting digit",
-           "message":"\"failed to parse select parameter (data->>-xy-4)\" (line 1, column 9)",
-           "code": "PGRST100",
-           "hint": null} |]
-        { matchStatus = 400, matchHeaders = [matchContentTypeJson] }
+  it "gives a meaningful error on bad syntax" $
+    get "/json_arr?select=data->>--34" `shouldRespondWith`
+      [json|
+        {"details": "unexpected \"-\" expecting digit",
+         "message": "\"failed to parse select parameter (data->>--34)\" (line 1, column 9)",
+         "code": "PGRST100",
+         "hint": null} |]
+      { matchStatus = 400, matchHeaders = [matchContentTypeJson] }
