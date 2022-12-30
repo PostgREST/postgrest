a101d27c9cf0611470b99e495e0c1d2852de8ff1
Robert <rob@vllmrt.net>
2022-12-30 15:40:17 +0100

    bump postgresql-libpq (#2599)
    For https://github.com/PostgREST/postgresql-libpq/pull/2.

--- nix/overlays/haskell-packages.nix
+++ nix/overlays/haskell-packages.nix
@@ -52,8 +52,8 @@ let
           (super.fetchFromGitHub {
             owner = "PostgREST";
             repo = "postgresql-libpq";
-            rev = "cef92cb4c07b56568dffdbf4b719258b82183119"; # master
-            sha256 = "0r59klrz47qcnd22s47h612mlz3jbg40wwalfj3f6djwg0cdyr85";
+            rev = "890a0a16cf57dd401420fdc6c7d576fb696003bc"; # master
+            sha256 = "1wmyhldk0k14y8whp1p4akrkqxf5snh8qsbm7fv5f7kz95nyffd0";
           })
           { });
     } // extraOverrides final prev;
