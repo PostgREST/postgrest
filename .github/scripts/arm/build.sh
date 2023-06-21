83cf15fb7edd76c9d33f5a7508b15bf05ec5a2f7
Laurence Isla <lau.isla.c@gmail.com>
2023-06-21 16:54:15 -0500

    ci: specify GHC_VERSION for ARM build on CI config
    * change cabal version from recommended to latest

--- .github/scripts/arm/build.sh
+++ .github/scripts/arm/build.sh
@@ -4,13 +4,15 @@
 
 [ -z "$1" ] && { echo "Missing 1st argument: PostgREST github commit SHA"; exit 1; }
 [ -z "$2" ] && { echo "Missing 2nd argument: Build environment directory name"; exit 1; }
+[ -z "$3" ] && { echo "Missing 3rd argument: GHC version"; exit 1; }
 
 PGRST_GITHUB_COMMIT="$1"
 SCRIPT_DIR="$2"
 
 DOCKER_BUILD_DIR="$SCRIPT_DIR/docker-env"
-CABAL_VERSION="3.6.0.0"
-GHC_VERSION="9.2.4"
+# latest is a shortcut documented on https://www.haskell.org/ghcup/guide/#tags-and-shortcuts
+CABAL_VERSION="latest"
+GHC_VERSION="$3"
 
 install_packages() {
   sudo apt-get update -y
