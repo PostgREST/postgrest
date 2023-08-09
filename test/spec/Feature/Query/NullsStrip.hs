279674d8ed29f19d244d078869e999381b2ef082
Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2023-08-09 11:28:33 +0500

    add tests for post and patch requests

--- test/spec/Feature/Query/NullsStrip.hs
+++ test/spec/Feature/Query/NullsStrip.hs
@@ -17,12 +17,30 @@ spec =
     let singularStrip = ("Accept", "application/vnd.pgrst.object+json;nulls=stripped")
 
     context "strip nulls from response" $ do
-      it "strips nulls when Accept: application/vnd.pgrst.array+json;nulls=stripped" $
+      it "strips nulls when Accept: application/vnd.pgrst.array+json;nulls=stripped" $ do
         request methodGet  "/organizations?select=*"
           [arrayStrip]
           ""
           `shouldRespondWith`
           [json|[{"id":1,"name":"Referee Org","manager_id":1},{"id":2,"name":"Auditor Org","manager_id":2},{"id":3,"name":"Acme","referee":1,"auditor":2,"manager_id":3},{"id":4,"name":"Umbrella","referee":1,"auditor":2,"manager_id":4},{"id":5,"name":"Cyberdyne","referee":3,"auditor":4,"manager_id":5},{"id":6,"name":"Oscorp","referee":3,"auditor":4,"manager_id":6}]|]
+          { matchStatus  = 200
+          , matchHeaders = [matchCTArrayStrip]
+          }
+
+        request methodPost  "/organizations?select=*"
+          [arrayStrip,("Prefer","return=representation")]
+          [json|{"id":7,"name":"John","referee":null,"auditor":null,"manager_id":6}|]
+          `shouldRespondWith`
+          [json|[{"id":7,"name":"John","manager_id":6}]|]
+          { matchStatus  = 201
+          , matchHeaders = [matchCTArrayStrip]
+          }
+
+        request methodPatch  "/organizations?id=eq.3&select=*"
+          [arrayStrip, ("Prefer","return=representation")]
+          [json|{"name":"John","referee":null}|]
+          `shouldRespondWith`
+          [json|[{"id":3,"name":"John","auditor":2,"manager_id":3}]|]
           { matchStatus  = 200
           , matchHeaders = [matchCTArrayStrip]
           }
@@ -48,12 +66,30 @@ spec =
           }
 
     context "strip nulls from response even if explicitly selected" $ do
-      it "strips nulls when Accept: application/vnd.pgrst.array+json;nulls=stripped" $
+      it "strips nulls when Accept: application/vnd.pgrst.array+json;nulls=stripped" $ do
         request methodGet  "/organizations?select=id,referee,auditor"
           [arrayStrip]
           ""
           `shouldRespondWith`
           [json|[{"id":1},{"id":2},{"id":3,"referee":1,"auditor":2},{"id":4,"referee":1,"auditor":2},{"id":5,"referee":3,"auditor":4},{"id":6,"referee":3,"auditor":4}]|]
+          { matchStatus  = 200
+          , matchHeaders = [matchCTArrayStrip]
+          }
+
+        request methodPost  "/organizations?select=id,referee,auditor"
+          [arrayStrip,("Prefer","return=representation")]
+          [json|{"id":7,"name":"John","referee":null,"auditor":null,"manager_id":6}|]
+          `shouldRespondWith`
+          [json|[{"id":7}]|]
+          { matchStatus  = 201
+          , matchHeaders = [matchCTArrayStrip]
+          }
+
+        request methodPatch  "/organizations?id=eq.3&select=id,name,referee,auditor"
+          [arrayStrip, ("Prefer","return=representation")]
+          [json|{"name":"John","referee":null}|]
+          `shouldRespondWith`
+          [json|[{"id":3,"name":"John","auditor":2}]|]
           { matchStatus  = 200
           , matchHeaders = [matchCTArrayStrip]
           }
