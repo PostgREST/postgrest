850b15fe13aa9881e0919a4beb1d27692192c88c
Laurence Isla <lau.isla.c@gmail.com>
2023-11-24 12:05:25 -0500

    fix: change timing name from 'query' to 'transaction'

--- src/PostgREST/Response/Performance.hs
+++ src/PostgREST/Response/Performance.hs
@@ -14,13 +14,13 @@ data ServerMetric =
     SMJwt
   | SMRender
   | SMPlan
-  | SMQuery
+  | SMTransaction
   deriving (Show, Eq, Ord)
 type ServerTimingData = Map ServerMetric (Maybe Double)
 
 -- | Render the Server-Timing header from a ServerTimingData
 --
--- >>> renderServerTimingHeader $ Map.fromList [(SMPlan, 0.1), (SMQuery, 0.2), (SMRender, 0.3), (SMJwt, 0.4)]
+-- >>> renderServerTimingHeader $ Map.fromList [(SMPlan, 0.1), (SMTransaction, 0.2), (SMRender, 0.3), (SMJwt, 0.4)]
 -- ("Server-Timing","jwt;dur=400000.0, render;dur=300000.0, plan;dur=100000.0, query;dur=200000.0")
 renderServerTimingHeader :: ServerTimingData -> HTTP.Header
 renderServerTimingHeader timingData =
@@ -28,7 +28,7 @@ renderServerTimingHeader timingData =
 renderTiming :: (ServerMetric, Maybe Double) -> BS.ByteString
 renderTiming (metric, time) = maybe "" (\x -> BS.concat [renderMetric metric, BS.pack $ ";dur=" <> showFFloat (Just 1) (x * 1000000) ""]) time
   where
-    renderMetric SMPlan   = "plan"
-    renderMetric SMQuery  = "query"
-    renderMetric SMRender = "render"
-    renderMetric SMJwt    = "jwt"
+    renderMetric SMPlan        = "plan"
+    renderMetric SMTransaction = "transaction"
+    renderMetric SMRender      = "render"
+    renderMetric SMJwt         = "jwt"
