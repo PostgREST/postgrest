cd85ac747d3fe25f7df52ee3d589f994058d12d5
Andrei Dziahel <develop7@develop7.info>
2024-01-02 17:38:28 +0100

    add: produce OpenTelemetry traces
    * Introduces producing OpenTelemetry traces with hs-opentelemetry.
    * Adds OTel spans over the whole application
      loop and over each request processing phase
    * Preliminary OTel tracing support in spec tests
    * Disables tracing in load and memory tests

--- src/PostgREST/AppState.hs
+++ src/PostgREST/AppState.hs
@@ -13,6 +13,7 @@ module PostgREST.AppState
   , getNextListenerDelay
   , getTime
   , getJwtCacheState
+  , getOTelTracer
   , init
   , initWithPool
   , putConfig -- For tests TODO refactoring
@@ -28,46 +29,52 @@ module PostgREST.AppState
   , isPending
   ) where
 
-import qualified Data.ByteString.Char8      as BS
-import           Data.Either.Combinators    (whenLeft)
-import qualified Hasql.Pool                 as SQL
-import qualified Hasql.Pool.Config          as SQL
-import qualified Hasql.Session              as SQL
-import qualified Hasql.Transaction.Sessions as SQL
-import qualified Network.HTTP.Types.Status  as HTTP
-import qualified PostgREST.Auth.JwtCache    as JwtCache
-import qualified PostgREST.Error            as Error
-import qualified PostgREST.Logger           as Logger
-import qualified PostgREST.Metrics          as Metrics
+import           Control.AutoUpdate                (defaultUpdateSettings,
+                                                    mkAutoUpdate,
+                                                    updateAction)
+import           Control.Debounce
+import           Control.Retry                     (RetryPolicy,
+                                                    RetryStatus (..),
+                                                    capDelay,
+                                                    exponentialBackoff,
+                                                    retrying,
+                                                    rsPreviousDelay)
+import qualified Data.ByteString.Char8             as BS
+import           Data.Either.Combinators           (whenLeft)
+import           Data.IORef                        (IORef,
+                                                    atomicWriteIORef,
+                                                    newIORef,
+                                                    readIORef)
+import           Data.Time.Clock                   (UTCTime,
+                                                    getCurrentTime)
+import qualified Hasql.Pool                        as SQL
+import qualified Hasql.Pool.Config                 as SQL
+import qualified Hasql.Session                     as SQL
+import qualified Hasql.Transaction.Sessions        as SQL
+import qualified Network.HTTP.Types.Status         as HTTP
+import           OpenTelemetry.Trace               (Tracer)
+import           PostgREST.Auth.JwtCache           (JwtCacheState,
+                                                    update)
+import qualified PostgREST.Auth.JwtCache           as JwtCache
+import           PostgREST.Config                  (AppConfig (..),
+                                                    addFallbackAppName,
+                                                    readAppConfig)
+import           PostgREST.Config.Database         (queryDbSettings,
+                                                    queryPgVersion,
+                                                    queryRoleSettings)
+import           PostgREST.Config.PgVersion        (PgVersion (..),
+                                                    minimumPgVersion)
+import qualified PostgREST.Error                   as Error
+import qualified PostgREST.Logger                  as Logger
+import qualified PostgREST.Metrics                 as Metrics
 import           PostgREST.Observation
-import           PostgREST.TimeIt           (timeItT)
-import           PostgREST.Version          (prettyVersion)
-
-import Control.AutoUpdate (defaultUpdateSettings, mkAutoUpdate,
-                           updateAction)
-import Control.Debounce
-import Control.Retry      (RetryPolicy, RetryStatus (..), capDelay,
-                           exponentialBackoff, retrying,
-                           rsPreviousDelay)
-import Data.IORef         (IORef, atomicWriteIORef, newIORef,
-                           readIORef)
-import Data.Time.Clock    (UTCTime, getCurrentTime)
-
-import PostgREST.Auth.JwtCache           (JwtCacheState, update)
-import PostgREST.Config                  (AppConfig (..),
-                                          addFallbackAppName,
-                                          readAppConfig)
-import PostgREST.Config.Database         (queryDbSettings,
-                                          queryPgVersion,
-                                          queryRoleSettings)
-import PostgREST.Config.PgVersion        (PgVersion (..),
-                                          minimumPgVersion)
-import PostgREST.SchemaCache             (SchemaCache (..),
-                                          querySchemaCache,
-                                          showSummary)
-import PostgREST.SchemaCache.Identifiers (quoteQi)
-
-import Protolude
+import           PostgREST.SchemaCache             (SchemaCache (..),
+                                                    querySchemaCache,
+                                                    showSummary)
+import           PostgREST.SchemaCache.Identifiers (quoteQi)
+import           PostgREST.TimeIt                  (timeItT)
+import           PostgREST.Version                 (prettyVersion)
+import           Protolude
 
 data AppState = AppState
   -- | Database connection pool
@@ -98,6 +105,10 @@ data AppState = AppState
   , stateJwtCache          :: JwtCache.JwtCacheState
   , stateLogger            :: Logger.LoggerState
   , stateMetrics           :: Metrics.MetricsState
+  -- | OpenTelemetry tracer. @Nothing@ represents disabled OTel SDK.
+  -- It's a workaround for now, as @hs-opentelemetry-api@ doesn't have @Tracer.tracerIsEnabled@ released yet.
+  -- Tracking issue: https://github.com/iand675/hs-opentelemetry/issues/212
+  , stateOTelTracer        :: Maybe Tracer
   }
 
 -- | Schema cache status.
@@ -106,8 +117,8 @@ newtype SchemaCacheStatus = SchemaCacheStatus
   { getSCStatusMVar :: MVar ()
   }
 
-init :: AppConfig -> IO AppState
-init conf@AppConfig{configLogLevel, configDbPoolSize} = do
+init :: AppConfig -> Maybe Tracer -> IO AppState
+init conf@AppConfig{configLogLevel, configDbPoolSize} tracer = do
   loggerState  <- Logger.init
   metricsState <- Metrics.init configDbPoolSize
   let observer = liftA2 (>>) (Logger.observationLogger loggerState configLogLevel) (Metrics.observationMetrics metricsState)
@@ -115,10 +126,10 @@ init conf@AppConfig{configLogLevel, configDbPoolSize} = do
   observer $ AppStartObs prettyVersion
 
   pool <- initPool conf observer
-  initWithPool pool conf loggerState metricsState observer --{ stateSocketREST = sock, stateSocketAdmin = adminSock}
+  initWithPool pool conf loggerState metricsState tracer observer
 
-initWithPool :: SQL.Pool -> AppConfig -> Logger.LoggerState -> Metrics.MetricsState -> ObservationHandler -> IO AppState
-initWithPool pool conf loggerState metricsState observer = do
+initWithPool :: SQL.Pool ->  AppConfig -> Logger.LoggerState -> Metrics.MetricsState -> Maybe Tracer -> ObservationHandler -> IO AppState
+initWithPool pool conf loggerState metricsState tracer observer = do
 
   appState <- AppState pool
     <$> newIORef minimumPgVersion -- assume we're in a supported version when starting, this will be corrected on a later step
@@ -135,6 +146,7 @@ initWithPool pool conf loggerState metricsState observer = do
     <*> JwtCache.init conf observer
     <*> pure loggerState
     <*> pure metricsState
+    <*> pure tracer
 
   deb <-
     let decisecond = 100000 in
@@ -261,6 +273,9 @@ getTime = stateGetTime
 
 getJwtCacheState :: AppState -> JwtCacheState
 getJwtCacheState = stateJwtCache
+
+getOTelTracer :: AppState -> Maybe Tracer
+getOTelTracer = stateOTelTracer
 
 getMainThreadId :: AppState -> ThreadId
 getMainThreadId = stateMainThreadId
