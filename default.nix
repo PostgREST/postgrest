d1ceffbc6239d90095ff1d11712f12fbd0956e11
Wolfgang Walther <walther@technowledgy.de>
2024-01-26 18:44:06 +0100

    nix: Build static executable on macos

--- default.nix
+++ default.nix
@@ -146,7 +146,7 @@ rec {
       inherit (pkgs.haskell.packages."${compiler}") hpc-codecov;
       inherit (pkgs.haskell.packages."${compiler}") weeder;
     };
-} // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux rec {
+
   # Static executable.
   inherit (staticHaskellPackage) postgrestStatic;
   inherit (staticHaskellPackage) packagesStatic;
