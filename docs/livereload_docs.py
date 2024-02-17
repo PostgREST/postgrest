c1c45060baeb439cd3b56cc79b831732cf4efb95
Wolfgang Walther <walther@technowledgy.de>
2024-02-17 12:53:41 +0100

    chore: Prepare merge of postgrest-docs into postgrest main repo
    This avoids some merge conflicts to allow git blame to detect renames properly.

--- docs/livereload_docs.py
+++ docs/livereload_docs.py
@@ -2,9 +2,9 @@
 from livereload import Server, shell
 from subprocess import call
 ## Build docs at startup
-call(['sphinx-build', '-b', 'html', '-a', '-n', 'docs', '_build'])
+call(['sphinx-build', '-b', 'html', '-a', '-n', '.', '_build'])
 server = Server()
-server.watch('docs/**/*.rst', shell('sphinx-build -b html -a -n docs _build'))
+server.watch('**/*.rst', shell('sphinx-build -b html -a -n . _build'))
 # For custom port and host
 # server.serve(root='_build/', host='192.168.1.2')
 server.serve(root='_build/')
