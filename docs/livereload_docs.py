311a38193b39d09be1a33ad7c70b8cd426f022a0
Wolfgang Walther <walther@technowledgy.de>
2024-02-17 12:57:18 +0100

    chore: Run postgrest-style on docs files
    This prevents CI from failing after the merge.

--- docs/livereload_docs.py
+++ docs/livereload_docs.py
@@ -4,15 +4,15 @@ from livereload import Server, shell
 from subprocess import call
 
 if len(sys.argv) == 1:
-    locale = 'default'
-    build = './build.sh'
+    locale = "default"
+    build = "./build.sh"
 else:
     locale = sys.argv[1]
-    build = f'./build.sh {locale}'
+    build = f"./build.sh {locale}"
 
 call(build, shell=True)
 
 server = Server()
-server.watch('**/*.rst', shell(build))
-server.watch(f'locales/{locale}/LC_MESSAGES/*.po', shell(build))
-server.serve(root=f'_build/html/{locale}')
+server.watch("**/*.rst", shell(build))
+server.watch(f"locales/{locale}/LC_MESSAGES/*.po", shell(build))
+server.serve(root=f"_build/html/{locale}")
