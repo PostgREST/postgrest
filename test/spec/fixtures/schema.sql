8ff9f3292e6eefa95e38cb4e2aaf72e3b5ead8c7
Wolfgang Walther <walther@technowledgy.de>
2024-02-19 17:49:13 +0100

    test: Hide "cast on domain" warnings in spec tests
    Also removes a few other unused settings which originate from running pg_dump.

--- test/spec/fixtures/schema.sql
+++ test/spec/fixtures/schema.sql
@@ -1,16 +1,7 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 9.5beta1
--- Dumped by pg_dump version 9.5beta1
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
 SET check_function_bodies = false;
-SET client_min_messages = warning;
+-- Hide warnings because casts on domains would show a lot of:
+--  WARNING:  cast will be ignored because the source data type is a domain
+SET client_min_messages = error;
 
 CREATE SCHEMA public;
 CREATE SCHEMA postgrest;
