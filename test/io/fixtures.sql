0d4441852e1c2383227f03ee189e563c611b0966
steve-chavez <stevechavezast@gmail.com>
2024-03-19 03:39:15 -0500

    test: notify do nothing

--- test/io/fixtures.sql
+++ test/io/fixtures.sql
@@ -89,6 +89,10 @@ begin
   perform pg_notify('pgrst', 'reload config');
 end $_$ volatile security definer language plpgsql ;
 
+create function notify_do_nothing() returns void as $_$
+  notify pgrst, 'nothing';
+$_$ language sql;
+
 create function reset_invalid_role_claim_key() returns void as $_$
 begin
   alter role postgrest_test_authenticator reset pgrst.jwt_role_claim_key;
