5ee71894d361a0af0078f8c42e45597b88061228
Laurence Isla <lau.isla.c@gmail.com>
2024-03-22 20:05:06 -0500

    test: fix some in-db config values
    To correctly test in-db override of config file values, the former must be different from the latter.

--- test/io/db_config.sql
+++ test/io/db_config.sql
@@ -1,31 +1,32 @@
 CREATE ROLE db_config_authenticator LOGIN NOINHERIT;
 
 -- reloadable config options
-ALTER ROLE db_config_authenticator SET pgrst.jwt_aud = 'https://example.org';
-ALTER ROLE db_config_authenticator SET pgrst.openapi_server_proxy_uri = 'https://example.org/api';
-ALTER ROLE db_config_authenticator SET pgrst.jwt_secret = 'REALLY=REALLY=REALLY=REALLY=VERY=SAFE';
-ALTER ROLE db_config_authenticator SET pgrst.jwt_secret_is_base64 = 'false';
-ALTER ROLE db_config_authenticator SET pgrst.jwt_role_claim_key = '."a"."role"';
-ALTER ROLE db_config_authenticator SET pgrst.jwt_cache_max_lifetime = '3600';
+-- these settings will override the values in configs/no-defaults.config, so they must be different
 ALTER ROLE db_config_authenticator SET pgrst.db_aggregates_enabled = 'false';
 ALTER ROLE db_config_authenticator SET pgrst.db_anon_role = 'anonymous';
-ALTER ROLE db_config_authenticator SET pgrst.db_tx_end = 'commit-allow-override';
-ALTER ROLE db_config_authenticator SET pgrst.db_pre_config = 'postgrest.preconf';
-ALTER ROLE db_config_authenticator SET pgrst.db_schemas = 'test, tenant1, tenant2';
-ALTER ROLE db_config_authenticator SET pgrst.db_root_spec = 'root';
-ALTER ROLE db_config_authenticator SET pgrst.db_plan_enabled = 'true';
-ALTER ROLE db_config_authenticator SET pgrst.db_prepared_statements = 'false';
-ALTER ROLE db_config_authenticator SET pgrst.db_pre_request = 'test.custom_headers';
-ALTER ROLE db_config_authenticator SET pgrst.db_max_rows = '1000';
 ALTER ROLE db_config_authenticator SET pgrst.db_extra_search_path = 'public, extensions';
+ALTER ROLE db_config_authenticator SET pgrst.db_max_rows = '500';
+ALTER ROLE db_config_authenticator SET pgrst.db_plan_enabled = 'false';
+ALTER ROLE db_config_authenticator SET pgrst.db_pre_config = 'postgrest.preconf';
+ALTER ROLE db_config_authenticator SET pgrst.db_pre_request = 'test.custom_headers';
+ALTER ROLE db_config_authenticator SET pgrst.db_prepared_statements = 'false';
+ALTER ROLE db_config_authenticator SET pgrst.db_root_spec = 'root';
+ALTER ROLE db_config_authenticator SET pgrst.db_schemas = 'test, tenant1, tenant2';
+ALTER ROLE db_config_authenticator SET pgrst.db_tx_end = 'commit-allow-override';
+ALTER ROLE db_config_authenticator SET pgrst.jwt_aud = 'https://example.org';
+ALTER ROLE db_config_authenticator SET pgrst.jwt_cache_max_lifetime = '3600';
+ALTER ROLE db_config_authenticator SET pgrst.jwt_role_claim_key = '."a"."role"';
+ALTER ROLE db_config_authenticator SET pgrst.jwt_secret = 'REALLY=REALLY=REALLY=REALLY=VERY=SAFE';
+ALTER ROLE db_config_authenticator SET pgrst.jwt_secret_is_base64 = 'false';
 ALTER ROLE db_config_authenticator SET pgrst.not_existing = 'should be ignored';
+ALTER ROLE db_config_authenticator SET pgrst.openapi_server_proxy_uri = 'https://example.org/api';
 ALTER ROLE db_config_authenticator SET pgrst.server_cors_allowed_origins = 'http://origin.com';
+ALTER ROLE db_config_authenticator SET pgrst.server_timing_enabled = 'false';
 ALTER ROLE db_config_authenticator SET pgrst.server_trace_header = 'CF-Ray';
-ALTER ROLE db_config_authenticator SET pgrst.server_timing_enabled = 'true';
 
 -- override with database specific setting
-ALTER ROLE db_config_authenticator IN DATABASE :DBNAME SET pgrst.jwt_secret = 'OVERRIDE=REALLY=REALLY=REALLY=REALLY=VERY=SAFE';
 ALTER ROLE db_config_authenticator IN DATABASE :DBNAME SET pgrst.db_extra_search_path = 'public, extensions, private';
+ALTER ROLE db_config_authenticator IN DATABASE :DBNAME SET pgrst.jwt_secret = 'OVERRIDE=REALLY=REALLY=REALLY=REALLY=VERY=SAFE';
 ALTER ROLE db_config_authenticator IN DATABASE :DBNAME SET pgrst.not_existing = 'should be ignored';
 
 -- other database settings that should be ignored
@@ -33,43 +34,44 @@ CREATE DATABASE other;
 ALTER ROLE db_config_authenticator IN DATABASE other SET pgrst.db_max_rows = '1111';
 
 -- non-reloadable configs
+ALTER ROLE db_config_authenticator SET pgrst.admin_server_port = 'ignored';
+ALTER ROLE db_config_authenticator SET pgrst.db_channel = 'ignored';
+ALTER ROLE db_config_authenticator SET pgrst.db_channel_enabled = 'ignored';
+ALTER ROLE db_config_authenticator SET pgrst.db_config = 'true';
+ALTER ROLE db_config_authenticator SET pgrst.db_pool = 'ignored';
+ALTER ROLE db_config_authenticator SET pgrst.db_pool_acquisition_timeout = 'ignored';
+ALTER ROLE db_config_authenticator SET pgrst.db_pool_timeout = 'ignored';
+ALTER ROLE db_config_authenticator SET pgrst.db_pool_max_idletime = 'ignored';
+ALTER ROLE db_config_authenticator SET pgrst.db_pool_max_lifetime = 'ignored';
+ALTER ROLE db_config_authenticator SET pgrst.db_uri = 'postgresql://ignored';
+ALTER ROLE db_config_authenticator SET pgrst.log_level = 'ignored';
 ALTER ROLE db_config_authenticator SET pgrst.server_host = 'ignored';
 ALTER ROLE db_config_authenticator SET pgrst.server_port = 'ignored';
 ALTER ROLE db_config_authenticator SET pgrst.server_unix_socket = 'ignored';
 ALTER ROLE db_config_authenticator SET pgrst.server_unix_socket_mode = 'ignored';
-ALTER ROLE db_config_authenticator SET pgrst.admin_server_port = 'ignored';
-ALTER ROLE db_config_authenticator SET pgrst.log_level = 'ignored';
-ALTER ROLE db_config_authenticator SET pgrst.db_uri = 'postgresql://ignored';
-ALTER ROLE db_config_authenticator SET pgrst.db_channel_enabled = 'ignored';
-ALTER ROLE db_config_authenticator SET pgrst.db_channel = 'ignored';
-ALTER ROLE db_config_authenticator SET pgrst.db_pool = 'ignored';
-ALTER ROLE db_config_authenticator SET pgrst.db_pool_timeout = 'ignored';
-ALTER ROLE db_config_authenticator SET pgrst.db_pool_acquisition_timeout = 'ignored';
-ALTER ROLE db_config_authenticator SET pgrst.db_pool_max_lifetime = 'ignored';
-ALTER ROLE db_config_authenticator SET pgrst.db_pool_max_idletime = 'ignored';
-ALTER ROLE db_config_authenticator SET pgrst.db_config = 'true';
 
 -- other authenticator reloadable config options
+-- these settings will override the values in configs/no-defaults.config, so they must be different
 CREATE ROLE other_authenticator LOGIN NOINHERIT;
-ALTER ROLE other_authenticator SET pgrst.jwt_aud = 'https://otherexample.org';
-ALTER ROLE other_authenticator SET pgrst.openapi_server_proxy_uri = 'https://otherexample.org/api';
-ALTER ROLE other_authenticator SET pgrst.jwt_secret = 'ODERREALLYREALLYREALLYREALLYVERYSAFE';
-ALTER ROLE other_authenticator SET pgrst.jwt_secret_is_base64 = 'true';
-ALTER ROLE other_authenticator SET pgrst.jwt_cache_max_lifetime = '7200';
 ALTER ROLE other_authenticator SET pgrst.db_aggregates_enabled = 'false';
-ALTER ROLE other_authenticator SET pgrst.db_schemas = 'test, other_tenant1, other_tenant2';
-ALTER ROLE other_authenticator SET pgrst.db_root_spec = 'other_root';
-ALTER ROLE other_authenticator SET pgrst.db_plan_enabled = 'true';
-ALTER ROLE other_authenticator SET pgrst.db_prepared_statements = 'false';
-ALTER ROLE other_authenticator SET pgrst.db_pre_request = 'test.other_custom_headers';
-ALTER ROLE other_authenticator SET pgrst.db_max_rows = '100';
 ALTER ROLE other_authenticator SET pgrst.db_extra_search_path = 'public, extensions, other';
+ALTER ROLE other_authenticator SET pgrst.db_max_rows = '100';
+ALTER ROLE other_authenticator SET pgrst.db_plan_enabled = 'true';
+ALTER ROLE other_authenticator SET pgrst.db_pre_config = 'postgrest.other_preconf';
+ALTER ROLE other_authenticator SET pgrst.db_pre_request = 'test.other_custom_headers';
+ALTER ROLE other_authenticator SET pgrst.db_prepared_statements = 'false';
+ALTER ROLE other_authenticator SET pgrst.db_root_spec = 'other_root';
+ALTER ROLE other_authenticator SET pgrst.db_schemas = 'test, other_tenant1, other_tenant2';
+ALTER ROLE other_authenticator SET pgrst.jwt_aud = 'https://otherexample.org';
+ALTER ROLE other_authenticator SET pgrst.jwt_secret = 'ODERREALLYREALLYREALLYREALLYVERYSAFE';
+ALTER ROLE other_authenticator SET pgrst.jwt_secret_is_base64 = 'false';
+ALTER ROLE other_authenticator SET pgrst.jwt_cache_max_lifetime = '7200';
 ALTER ROLE other_authenticator SET pgrst.openapi_mode = 'disabled';
 ALTER ROLE other_authenticator SET pgrst.openapi_security_active = 'false';
+ALTER ROLE other_authenticator SET pgrst.openapi_server_proxy_uri = 'https://otherexample.org/api';
 ALTER ROLE other_authenticator SET pgrst.server_cors_allowed_origins = 'http://otherorigin.com';
-ALTER ROLE other_authenticator SET pgrst.server_trace_header = 'traceparent';
-ALTER ROLE other_authenticator SET pgrst.db_pre_config = 'postgrest.pre_config';
 ALTER ROLE other_authenticator SET pgrst.server_timing_enabled = 'true';
+ALTER ROLE other_authenticator SET pgrst.server_trace_header = 'traceparent';
 
 create schema postgrest;
 grant usage on schema postgrest to db_config_authenticator;
@@ -94,6 +96,12 @@ create or replace function postgrest.preconf()
 returns void as $$
 begin
   null;
+end $$ language plpgsql;
+
+create or replace function postgrest.other_preconf()
+returns void as $$
+begin
+  perform postgrest.pre_config();
 end $$ language plpgsql;
 
 -- authenticator used for tests that manipulate statement timeout
