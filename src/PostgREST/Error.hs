9d3e4ba4a138370a3a3a200906fd0500c846f6d5
Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2024-05-18 23:31:00 +0500

    fix: fix incorrect 413 error on pg 54* errors

--- src/PostgREST/Error.hs
+++ src/PostgREST/Error.hs
@@ -491,7 +491,7 @@ pgErrorStatus authed (SQL.SessionUsageError (SQL.QueryError _ _ (SQL.ResultError
         '4':'0':_ -> HTTP.status500 -- tx rollback
         "53400"   -> HTTP.status500 -- config limit exceeded
         '5':'3':_ -> HTTP.status503 -- insufficient resources
-        '5':'4':_ -> HTTP.status413 -- too complex
+        '5':'4':_ -> HTTP.status500 -- too complex
         '5':'5':_ -> HTTP.status500 -- obj not on prereq state
         "57P01"   -> HTTP.status503 -- terminating connection due to administrator command
         '5':'7':_ -> HTTP.status500 -- operator intervention
--- test/spec/Feature/Query/ErrorSpec.hs
+++ test/spec/Feature/Query/ErrorSpec.hs
@@ -67,3 +67,15 @@ pgErrorCodeMapping = do
   describe "PostreSQL error code mappings" $ do
     it "should return 500 for cardinality_violation" $
       get "/bad_subquery" `shouldRespondWith` 500
+
+    it "should return 500 for statement too complex" $
+      request methodPost "/infinite_inserts"
+        []
+        [json|{"id": 3, "name": "qwer"}|]
+        `shouldRespondWith`
+        [json|
+          {"code": "54001",
+           "details": null,
+           "hint": "Increase the configuration parameter \"max_stack_depth\" (currently 2048kB), after ensuring the platform's stack depth limit is adequate.",
+            "message": "stack depth limit exceeded"}|]
+        { matchStatus = 500 }
