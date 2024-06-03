a46bea16e239d120804d7d125f1f737e4bfb3b8d
Joel Jakobsson <joel@compiler.org>
2024-06-03 10:24:35 +0200

    nix: fix slocat overlay with nix 2.22
    This seems to happen on nix 2.22 only, v2.21 in CI and v2.20 locally work fine. The error is:
    
    vendor folder is empty, please set 'vendorHash = null;' in your expression
    For full logs, run 'nix-store -l /nix/store/kxnnr344n7gsxzc6kycj19hs19rvddjj-slocat-go-modules.drv'.
    error: 1 dependencies of derivation '/nix/store/x9w480k36a11i99m6zp12d5cjijsn3lm-slocat.drv' failed to build
    
    Since the slocat module doesn't actually have any dependencies, this shouldn't matter much.

--- nix/overlays/slocat.nix
+++ nix/overlays/slocat.nix
@@ -8,6 +8,6 @@ _: prev:
       rev = "52e7512c6029fd00483e41ccce260a3b4b9b3b64";
       sha256 = "sha256-qn6luuh5wqREu3s8RfuMCP5PKdS2WdwPrujRYTpfzQ8=";
     };
-    vendorHash = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+    vendorHash = null;
   };
 }
