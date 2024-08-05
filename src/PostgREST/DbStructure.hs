d77915d7f3db4064e5d14bcfde84626ffd9fdb13
Jüri Tali <114475833+jyritali@users.noreply.github.com>
2024-08-05 13:07:34 +0300

    correct control user rights sql where clauses

--- src/PostgREST/DbStructure.hs
+++ src/PostgREST/DbStructure.hs
@@ -597,7 +597,7 @@ tablesSqlQuery getAll pgVer =
     (
       (
       c.relkind IN ('r','p')
-      AND exists(SELECT * FROM information_schema.table_privileges where table_name=c.relname and privilege_type='INSERT' and grantee=current_user)
+      AND exists(SELECT * FROM information_schema.table_privileges where table_schema=n.nspname and table_name=c.relname and privilege_type='INSERT' and grantee=current_user)
       )
       OR (
         c.relkind in ('v','f')
@@ -605,31 +605,31 @@ tablesSqlQuery getAll pgVer =
         -- corresponds to `1 << CMD_INSERT` in the PostgreSQL source code, i.e.
         -- it's possible to insert into the relation.
         AND (pg_relation_is_updatable(c.oid::regclass, TRUE) & 8) = 8
-        AND exists(SELECT * FROM information_schema.table_privileges where table_name=c.relname and privilege_type='INSERT' and grantee=current_user)
+        AND exists(SELECT * FROM information_schema.table_privileges where table_schema=n.nspname and table_name=c.relname and privilege_type='INSERT' and grantee=current_user)
       )
     ) AS insertable,
     (
       (
       c.relkind IN ('r','p')
-      AND exists(SELECT * FROM information_schema.table_privileges where table_name=c.relname and privilege_type='UPDATE' and grantee=current_user)
+      AND exists(SELECT * FROM information_schema.table_privileges where table_schema=n.nspname and table_name=c.relname and privilege_type='UPDATE' and grantee=current_user)
       )
       OR (
         c.relkind in ('v','f')
         -- CMD_UPDATE
         AND (pg_relation_is_updatable(c.oid::regclass, TRUE) & 4) = 4
-        AND exists(SELECT * FROM information_schema.table_privileges where table_name=c.relname and privilege_type='UPDATE' and grantee=current_user)
+        AND exists(SELECT * FROM information_schema.table_privileges where table_schema=n.nspname and table_name=c.relname and privilege_type='UPDATE' and grantee=current_user)
       )
     ) AS updatable,
     (
       (
       c.relkind IN ('r','p')
-      AND exists(SELECT * FROM information_schema.table_privileges where table_name=c.relname and privilege_type='DELETE' and grantee=current_user)
+      AND exists(SELECT * FROM information_schema.table_privileges where table_schema=n.nspname and table_name=c.relname and privilege_type='DELETE' and grantee=current_user)
       )
       OR (
         c.relkind in ('v','f')
         -- CMD_DELETE
         AND (pg_relation_is_updatable(c.oid::regclass, TRUE) & 16) = 16
-        AND exists(SELECT * FROM information_schema.table_privileges where table_name=c.relname and privilege_type='DELETE' and grantee=current_user)
+        AND exists(SELECT * FROM information_schema.table_privileges where table_schema=n.nspname and table_name=c.relname and privilege_type='DELETE' and grantee=current_user)
       )
     ) AS deletable,
     coalesce(tpks.pk_cols, '{}') as pk_cols,
