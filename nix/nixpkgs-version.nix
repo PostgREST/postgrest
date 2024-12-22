d75bb73d65da4d88945f7b04c12371144ed86820
Wolfgang Walther <walther@technowledgy.de>
2024-12-22 19:10:22 +0100

    chore(deps): update nixpkgs to unstable 2025-01-11

--- nix/nixpkgs-version.nix
+++ nix/nixpkgs-version.nix
@@ -2,8 +2,8 @@
 {
   owner = "NixOS";
   repo = "nixpkgs";
-  ref = "refs/heads/nixpkgs-unstable-darwin";
-  date = "2024-11-09";
-  rev = "a90280100f41a10914edfe729a4053e60c92b8e3";
-  tarballHash = "1vwr665b6l6gma24w45q5hic86vbd8alc01mziwwr621hwlca88f";
+  ref = "refs/heads/nixpkgs-unstable";
+  date = "2025-01-11";
+  rev = "32af3611f6f05655ca166a0b1f47b57c762b5192";
+  tarballHash = "0shknvd56nfqh4awklgsxwaavpfixgh766m428qdlxihjmmqvhbl";
 }
