067c181bb8ab7ed905a2115cb16b575dca697845
Wolfgang Walther <walther@technowledgy.de>
2024-12-23 22:04:05 +0100

    nix: remove libpq overlay
    We have fixed the static build of PostgreSQL upstream, so we don't need
    our separate overlay anymore. One more step towards building a static
    executable on other platforms.

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -3,6 +3,5 @@
   checked-shell-script = import ./checked-shell-script;
   gitignore = import ./gitignore.nix;
   haskell-packages = import ./haskell-packages.nix;
-  postgresql-libpq = import ./postgresql-libpq.nix;
   slocat = import ./slocat.nix;
 }
