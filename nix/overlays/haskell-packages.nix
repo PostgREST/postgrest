c026d0752fbe47070d073f9960ba2ea8f6647206
Diogo Biazus <diogo@biazus.ca>
2025-02-03 09:40:37 -0500

    fix: Fix regression for schema cache reloading via NOTIFY on Windows
    Upstream accidentally removed the fix, which was introduced for #2524. Fixed again.
    
    (cherry picked from commit b285f5fba6f31ae127b6e2a10c17790f419f5d88)

--- nix/overlays/haskell-packages.nix
+++ nix/overlays/haskell-packages.nix
@@ -74,8 +74,8 @@ let
       hasql-notifications = lib.dontCheck (prev.callHackageDirect
         {
           pkg = "hasql-notifications";
-          ver = "0.2.2.0";
-          sha256 = "sha256-73OQ9/su2qvO7HavF3xuuNWLXSXyB9reBUQDaHys06I=";
+          ver = "0.2.2.2";
+          sha256 = "sha256-myKwlug7OgTa/qP6mHfCD+5Q8IhM17JvpJBfSo+M01k=";
         }
         { }
       );
