bf5aaa848237656eed8dfb64c473958f2729c4c3
Fauzan <fauzanfauh45@gmail.com>
2025-04-10 21:09:51 +0700

    fix: should be application/sql instead of json

--- test/spec/Feature/Query/RawSQLSpec.hs
+++ test/spec/Feature/Query/RawSQLSpec.hs
@@ -19,25 +19,25 @@ spec = describe "When accept header is set to application/sql" $ do
     r <- request methodGet "/items?id=eq.1" mtApplicationSQLHdrs ""
     liftIO $ do
       simpleStatus r `shouldBe` status200
-      simpleHeaders r `shouldContain` [("Content-Type", "application/json; charset=utf-8")]
+      simpleHeaders r `shouldContain` [("Content-Type", "application/sql; charset=utf-8")]
 
   it "responds raw sql to a POST request for insert" $ do
     r <- request methodPost "/projects"
              (acceptHdrs "application/sql") [json|{"id":100, "name": "Project 100"}|]
     liftIO $ do
       simpleStatus r `shouldBe` status200
-      simpleHeaders r `shouldContain` [("Content-Type", "application/json; charset=utf-8")]
+      simpleHeaders r `shouldContain` [("Content-Type", "application/sql; charset=utf-8")]
 
   it "responds raw sql to a create POST for update" $ do
     r <- request methodPatch "/projects?id=eq.3"
              (acceptHdrs "application/sql") [json|{"name": "Patched Project"}|]
     liftIO $ do
       simpleStatus r `shouldBe` status200
-      simpleHeaders r `shouldContain` [("Content-Type", "application/json; charset=utf-8")]
+      simpleHeaders r `shouldContain` [("Content-Type", "application/sql; charset=utf-8")]
 
   it "responds raw sql to a create POST for delete" $ do
     r <- request methodDelete "/projects?id=in.(1,2,3)"
              (acceptHdrs "application/sql") ""
     liftIO $ do
       simpleStatus r `shouldBe` status200
-      simpleHeaders r `shouldContain` [("Content-Type", "application/json; charset=utf-8")]
+      simpleHeaders r `shouldContain` [("Content-Type", "application/sql; charset=utf-8")]
