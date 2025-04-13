ff87f2372bb034283f840bd81b0a81a3d3e7bf70
Fauzan <fauzanfauh45@gmail.com>
2025-04-13 21:58:18 +0700

    test: add testing for application/json+sql request

--- test/spec/Feature/Query/RawSQLSpec.hs
+++ test/spec/Feature/Query/RawSQLSpec.hs
@@ -12,32 +12,32 @@ import Protolude
 import SpecHelper (acceptHdrs)
 
 spec :: SpecWith ((), Application)
-spec = describe "When accept header is set to application/sql" $ do
-  let mtApplicationSQLHdrs = acceptHdrs "application/sql"
+spec = describe "When accept header is set to application/json+sql" $ do
+  let mtApplicationJSONSQLHdrs = acceptHdrs "application/json+sql"
 
   it "responds raw sql to a GET request" $ do
-    r <- request methodGet "/items?id=eq.1" mtApplicationSQLHdrs ""
+    r <- request methodGet "/items?id=eq.1" mtApplicationJSONSQLHdrs ""
     liftIO $ do
       simpleStatus r `shouldBe` status200
-      simpleHeaders r `shouldContain` [("Content-Type", "application/sql; charset=utf-8")]
+      simpleHeaders r `shouldContain` [("Content-Type", "application/json; charset=utf-8")]
 
   it "responds raw sql to a POST request for insert" $ do
     r <- request methodPost "/projects"
-             (acceptHdrs "application/sql") [json|{"id":100, "name": "Project 100"}|]
+             mtApplicationJSONSQLHdrs [json|{"id":100, "name": "Project 100"}|]
     liftIO $ do
       simpleStatus r `shouldBe` status200
-      simpleHeaders r `shouldContain` [("Content-Type", "application/sql; charset=utf-8")]
+      simpleHeaders r `shouldContain` [("Content-Type", "application/json; charset=utf-8")]
 
   it "responds raw sql to a create POST for update" $ do
     r <- request methodPatch "/projects?id=eq.3"
-             (acceptHdrs "application/sql") [json|{"name": "Patched Project"}|]
+             mtApplicationJSONSQLHdrs [json|{"name": "Patched Project"}|]
     liftIO $ do
       simpleStatus r `shouldBe` status200
-      simpleHeaders r `shouldContain` [("Content-Type", "application/sql; charset=utf-8")]
+      simpleHeaders r `shouldContain` [("Content-Type", "application/json; charset=utf-8")]
 
   it "responds raw sql to a create POST for delete" $ do
     r <- request methodDelete "/projects?id=in.(1,2,3)"
-             (acceptHdrs "application/sql") ""
+             mtApplicationJSONSQLHdrs ""
     liftIO $ do
       simpleStatus r `shouldBe` status200
-      simpleHeaders r `shouldContain` [("Content-Type", "application/sql; charset=utf-8")]
+      simpleHeaders r `shouldContain` [("Content-Type", "application/json; charset=utf-8")]
