2bab8969f0023de610929374c928c06297528800
Taimoor Zaeem <taimoorzaeem@gmail.com>
2025-07-17 21:15:26 +0500

    refactor(auth): declutter jwt module
    In a previous refactor, unnecessary changes like introducing
    type class constraints, qualified constraints, language extensions
    were added.
    
    This made the code much harder to read and debug. This commit
    refactors and removes the unnecessary code to debug new issues
    related to authentication modules. Also reduces the lines of code.

--- src/PostgREST/Auth/Jwt.hs
+++ src/PostgREST/Auth/Jwt.hs
@@ -4,20 +4,19 @@ Description : PostgREST JWT support functions.
 
 This module provides functions to deal with JWT parsing and validation (http://jwt.io).
 -}
-{-# LANGUAGE DeriveGeneric         #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE ImpredicativeTypes    #-}
-{-# LANGUAGE LambdaCase            #-}
-{-# LANGUAGE NamedFieldPuns        #-}
-{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE LambdaCase      #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module PostgREST.Auth.Jwt
-  ( parseAndDecodeClaims
-  , parseClaims) where
+  ( parseToken
+  , parseClaims
+  )
+  where
 
 import qualified Data.Aeson                 as JSON
 import qualified Data.Aeson.Key             as K
 import qualified Data.Aeson.KeyMap          as KM
+import qualified Data.Aeson.Types           as JSON
 import qualified Data.ByteString            as BS
 import qualified Data.ByteString.Internal   as BS
 import qualified Data.ByteString.Lazy.Char8 as LBS
@@ -29,87 +28,35 @@ import qualified Jose.Jwt                   as JWT
 
 import Control.Monad.Except    (liftEither)
 import Data.Either.Combinators (mapLeft)
-import Data.Text               ()
 import Data.Time.Clock         (UTCTime, nominalDiffTimeToSeconds)
 import Data.Time.Clock.POSIX   (utcTimeToPOSIXSeconds)
 
 import PostgREST.Auth.Types (AuthResult (..))
 import PostgREST.Config     (AppConfig (..), FilterExp (..), JSPath,
                              JSPathExp (..))
-import PostgREST.Error      (Error (..),
-                             JwtClaimsError (AudClaimNotStringOrArray, ExpClaimNotNumber, IatClaimNotNumber, JWTExpired, JWTIssuedAtFuture, JWTNotInAudience, JWTNotYetValid, NbfClaimNotNumber, ParsingClaimsFailed),
+import PostgREST.Error      (Error (..), JwtClaimsError (..),
                              JwtDecodeError (..), JwtError (..))
 
-import Data.Aeson       ((.:?))
-import Data.Aeson.Types (parseMaybe)
-import Jose.Jwk         (JwkSet)
-import Protolude        hiding (first)
+import Protolude hiding (first)
 
-parseAndDecodeClaims :: (MonadError Error m, MonadIO m) => JwkSet -> ByteString -> m JSON.Object
-parseAndDecodeClaims jwkSet token = parseToken jwkSet token >>= decodeClaims
-
-decodeClaims :: MonadError Error m => JWT.JwtContent -> m JSON.Object
-decodeClaims (JWT.Jws (_, claims)) = maybe (throwError (JwtErr $ JwtClaimsErr ParsingClaimsFailed)) pure (JSON.decodeStrict claims)
-decodeClaims _ = throwError $ JwtErr $ JwtDecodeErr UnsupportedTokenType
-
-validateClaims :: MonadError Error m => UTCTime -> Maybe Text -> JSON.Object -> m ()
-validateClaims time getConfigAud claims = liftEither $ maybeToLeft () (fmap JwtErr . getAlt $ JwtClaimsErr <$> checkForErrors time getConfigAud claims)
-
-data ValidAud = VANull | VAString Text | VAArray [Text] deriving Generic
-instance JSON.FromJSON ValidAud where
-  parseJSON JSON.Null = pure VANull
-  parseJSON o = JSON.genericParseJSON JSON.defaultOptions { JSON.sumEncoding = JSON.UntaggedValue } o
-
-checkForErrors :: (Monad m, forall a. Monoid (m a)) => UTCTime -> Maybe Text -> JSON.Object -> m JwtClaimsError
-checkForErrors time cfgAud = mconcat
-  [
-    claim "exp" ExpClaimNotNumber $ inThePast JWTExpired
-  , claim "nbf" NbfClaimNotNumber $ inTheFuture JWTNotYetValid
-  , claim "iat" IatClaimNotNumber $ inTheFuture JWTIssuedAtFuture
-  , claim "aud" AudClaimNotStringOrArray checkAud
-  ]
-  where
-      allowedSkewSeconds = 30 :: Int64
-      sciToInt = fromMaybe 0 . Sci.toBoundedInteger
-      toSec = floor . nominalDiffTimeToSeconds . utcTimeToPOSIXSeconds
-      now = toSec time
-
-      inTheFuture = checkTime ((now + allowedSkewSeconds) <)
-      inThePast = checkTime ((now - allowedSkewSeconds) >)
-
-      checkTime cond = checkValue (cond. sciToInt)
-
-      checkAud = \case
-        (VAString aud)                     -> liftMaybe cfgAud >>= checkValue (aud /=) JWTNotInAudience
-        (VAArray auds) | (not . null) auds -> liftMaybe cfgAud >>= checkValue (not . (`elem` auds)) JWTNotInAudience
-        _                                  -> mempty
-
-      liftMaybe = maybe mempty pure
-
-      checkValue invalid msg val =
-        if invalid val then
-          pure msg
-        else
-          mempty
-
-      claim key parseError checkParsed = maybe (pure parseError) (maybe mempty checkParsed) . parseMaybe (.:? key)
+data JwtTimeClaim = Exp | Iat | Nbf
 
 -- | Receives the JWT secret and audience (from config) and a JWT and returns a
 -- JSON object of JWT claims.
-parseToken :: (MonadError Error m, MonadIO m) => JwkSet -> ByteString -> m JWT.JwtContent
-parseToken _ "" = throwError $ JwtErr $ JwtDecodeErr EmptyAuthHeader
-parseToken secret tkn = do
-  -- secret <- liftEither . maybeToRight (JwtErr JwtSecretMissing) $ configJWKS
-  tknWith3Parts <- hasThreeParts tkn
+parseToken :: AppConfig -> Maybe ByteString -> UTCTime -> ExceptT Error IO JSON.Value
+parseToken _ Nothing _ = return JSON.emptyObject
+parseToken _ (Just "") _ = throwE . JwtErr $ JwtDecodeErr EmptyAuthHeader
+parseToken AppConfig{..} (Just tkn) time = do
+  secret <- liftEither . maybeToRight (JwtErr JwtSecretMissing) $ configJWKS
+  tknWith3Parts <- liftEither $ hasThreeParts tkn
   eitherContent <- liftIO $ JWT.decode (JWT.keys secret) Nothing tknWith3Parts
-  liftEither . mapLeft (JwtErr . jwtDecodeError) $ eitherContent
-  --liftEither $ mapLeft JwtErr $ verifyClaims content
+  content <- liftEither . mapLeft (JwtErr . jwtDecodeError) $ eitherContent
+  liftEither $ mapLeft JwtErr $ verifyClaims content
   where
-      --hasThreeParts :: ByteString -> Either Error ByteString
+      hasThreeParts :: ByteString -> Either Error ByteString
       hasThreeParts token = case length $ BS.split (BS.c2w '.') token of
-        3 -> pure token
-        n -> throwError $ JwtErr $ JwtDecodeErr $ UnexpectedParts n
-
+        3 -> Right token
+        n -> Left $ JwtErr $ JwtDecodeErr $ UnexpectedParts n
       jwtDecodeError :: JWT.JwtError -> JwtError
       -- The only errors we can get from JWT.decode function are:
       --   BadAlgorithm
@@ -121,26 +68,66 @@ parseToken secret tkn = do
       -- Control never reaches here, the decode function only returns the above three
       jwtDecodeError _                    = JwtDecodeErr UnreachableDecodeError
 
-parseClaims :: (MonadError Error m, MonadIO m) => AppConfig -> UTCTime -> JSON.Object -> m AuthResult
-parseClaims AppConfig{configJwtAudience, configJwtRoleClaimKey, configDbAnonRole} time mclaims = do
-  validateClaims time configJwtAudience mclaims
+      verifyClaims :: JWT.JwtContent -> Either JwtError JSON.Value
+      verifyClaims (JWT.Jws (_, claims)) = case JSON.decodeStrict claims of
+        Just jclaims@(JSON.Object mclaims) ->
+          verifyClaim mclaims "exp" (isValidTimeClaim Exp) JWTExpired >>
+          verifyClaim mclaims "nbf" (isValidTimeClaim Nbf) JWTNotYetValid >>
+          verifyClaim mclaims "iat" (isValidTimeClaim Iat) JWTIssuedAtFuture >>
+          verifyClaim mclaims "aud" isValidAudClaim JWTNotInAudience >>
+          return jclaims
+        _ -> Left $ JwtClaimsErr ParsingClaimsFailed
+      -- TODO: We could enable JWE support here (encrypted tokens)
+      verifyClaims _ = Left $ JwtDecodeErr UnsupportedTokenType
+
+      verifyClaim mclaims claim isValidFunc err = do
+        isValid <- maybe (Right True) isValidFunc (KM.lookup claim mclaims)
+        unless isValid $ Left $ JwtClaimsErr err
+
+      allowedSkewSeconds = 30 :: Int64
+      now = floor . nominalDiffTimeToSeconds $ utcTimeToPOSIXSeconds time
+      sciToInt = fromMaybe 0 . Sci.toBoundedInteger
+      allStrings = all (\case (JSON.String _) -> True; _ -> False)
+
+      isValidTimeClaim ::  JwtTimeClaim -> JSON.Value -> Either JwtError Bool
+      isValidTimeClaim jwtClaim (JSON.Number secs) = case jwtClaim of
+        Exp -> Right $ (now - allowedSkewSeconds) < sciToInt secs
+        Nbf -> Right $ (now + allowedSkewSeconds) > sciToInt secs
+        Iat -> Right $ (now + allowedSkewSeconds) > sciToInt secs
+      isValidTimeClaim jwtClaim _ = case jwtClaim of
+        Exp -> Left $ JwtClaimsErr ExpClaimNotNumber
+        Nbf -> Left $ JwtClaimsErr NbfClaimNotNumber
+        Iat -> Left $ JwtClaimsErr IatClaimNotNumber
+
+      isValidAudClaim :: JSON.Value -> Either JwtError Bool
+      isValidAudClaim JSON.Null = Right True -- {"aud": null} is valid for all audiences
+      isValidAudClaim (JSON.String str) = Right $ maybe (const True) (==) configJwtAudience str
+      isValidAudClaim (JSON.Array arr)
+        | null arr = Right True              -- {"aud": []} is valid for all audiences
+        | allStrings arr = Right $ maybe True (\a -> JSON.String a `elem` arr) configJwtAudience
+      isValidAudClaim _ = Left $ JwtClaimsErr AudClaimNotStringOrArray
+
+parseClaims :: AppConfig -> JSON.Value -> ExceptT Error IO AuthResult
+parseClaims AppConfig{..} jclaims@(JSON.Object mclaims) = do
   -- role defaults to anon if not specified in jwt
   role <- liftEither . maybeToRight (JwtErr JwtTokenRequired) $
-    unquoted <$> walkJSPath (Just $ JSON.Object mclaims) configJwtRoleClaimKey <|> configDbAnonRole
-  pure AuthResult
-           { authClaims = mclaims & KM.insert "role" (JSON.toJSON $ decodeUtf8 role)
-           , authRole = role
-           }
+    unquoted <$> walkJSPath (Just jclaims) configJwtRoleClaimKey <|> configDbAnonRole
+  return AuthResult {
+      authClaims = mclaims & KM.insert "role" (JSON.toJSON $ decodeUtf8 role)
+    , authRole = role
+    }
   where
     walkJSPath :: Maybe JSON.Value -> JSPath -> Maybe JSON.Value
     walkJSPath x                      []                = x
     walkJSPath (Just (JSON.Object o)) (JSPKey key:rest) = walkJSPath (KM.lookup (K.fromText key) o) rest
     walkJSPath (Just (JSON.Array ar)) (JSPIdx idx:rest) = walkJSPath (ar V.!? idx) rest
-    walkJSPath (Just (JSON.Array ar)) [JSPFilter (EqualsCond txt)] = findFirstMatch (==) txt ar
-    walkJSPath (Just (JSON.Array ar)) [JSPFilter (NotEqualsCond txt)] = findFirstMatch (/=) txt ar
-    walkJSPath (Just (JSON.Array ar)) [JSPFilter (StartsWithCond txt)] = findFirstMatch T.isPrefixOf txt ar
-    walkJSPath (Just (JSON.Array ar)) [JSPFilter (EndsWithCond txt)] = findFirstMatch T.isSuffixOf txt ar
-    walkJSPath (Just (JSON.Array ar)) [JSPFilter (ContainsCond txt)] = findFirstMatch T.isInfixOf txt ar
+    walkJSPath (Just (JSON.Array ar)) [JSPFilter filt] =
+      case filt of
+        EqualsCond txt     -> findFirstMatch (==) txt ar
+        NotEqualsCond txt  -> findFirstMatch (/=) txt ar
+        StartsWithCond txt -> findFirstMatch T.isPrefixOf txt ar
+        EndsWithCond txt   -> findFirstMatch T.isSuffixOf txt ar
+        ContainsCond txt   -> findFirstMatch T.isInfixOf txt ar
     walkJSPath _                      _                 = Nothing
 
     findFirstMatch matchWith pattern = foldr checkMatch Nothing
@@ -153,3 +140,5 @@ parseClaims AppConfig{configJwtAudience, configJwtRoleClaimKey, configDbAnonRole
     unquoted :: JSON.Value -> BS.ByteString
     unquoted (JSON.String t) = encodeUtf8 t
     unquoted v               = LBS.toStrict $ JSON.encode v
+-- impossible case - just added to please -Wincomplete-patterns
+parseClaims _ _ = return AuthResult { authClaims = KM.empty, authRole = mempty }
