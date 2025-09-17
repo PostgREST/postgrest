f299de57ae227b9acfb1630efef2e81ad11a76c6
Taimoor Zaeem <taimoorzaeem@gmail.com>
2025-09-17 18:17:29 +0500

    test(io): remove unused duplicate freeport() function
    Signed-off-by: Taimoor Zaeem <taimoorzaeem@gmail.com>

--- test/io/util.py
+++ test/io/util.py
@@ -24,14 +24,6 @@ class Thread(threading.Thread):
             raise self._exception
 
 
-def freeport():
-    "Find a free port on localhost."
-    with contextlib.closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-        s.bind(("", 0))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        return s.getsockname()[1]
-
-
 def authheader(token):
     "Bearer token HTTP authorization header."
     return {"Authorization": f"Bearer {token}"}
