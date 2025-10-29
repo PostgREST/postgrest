1c9d633e941406e4c4b60fdb438bb568f0899038
Michał Kłeczek <michal@kleczek.org>
2025-10-29 10:27:45 +0100

    change: Make jwt-aud config value a regular expression
    This change adds flexibility to aud claim validation. jwt-aud configuration property can now be specified as a regular expression. For example, it is now possible to
    * configure multiple acceptable aud values with '|' regex operator, eg: 'audience1|audience2|audience3'
    * accept any audience from a particular domain, eg: 'https://[a-z0-9]*\.example\.com'

--- src/PostgREST/Config.hs
+++ src/PostgREST/Config.hs
@@ -19,6 +19,7 @@ module PostgREST.Config
   , LogLevel(..)
   , OpenAPIMode(..)
   , Proxy(..)
+  , CfgAud
   , toText
   , isMalformedProxyUri
   , readAppConfig
@@ -29,6 +30,8 @@ module PostgREST.Config
   , addTargetSessionAttrs
   , exampleConfigFile
   , audMatchesCfg
+  , defaultCfgAud
+  , parseCfgAud
   ) where
 
 import qualified Data.Aeson             as JSON
@@ -50,7 +53,7 @@ import Data.List.NonEmpty      (fromList, toList)
 import Data.Maybe              (fromJust)
 import Data.Scientific         (floatingOrInteger)
 import Jose.Jwk                (Jwk, JwkSet)
-import Network.URI             (escapeURIString, isURI,
+import Network.URI             (escapeURIString,
                                 isUnescapedInURIComponent)
 import Numeric                 (readOct, showOct)
 import System.Environment      (getEnvironment)
@@ -66,10 +69,31 @@ import PostgREST.Config.Proxy            (Proxy (..),
 import PostgREST.SchemaCache.Identifiers (QualifiedIdentifier, dumpQi,
                                           toQi)
 
-import Protolude hiding (Proxy, toList)
+import           Protolude       hiding (Proxy, toList)
+import qualified Text.Regex.TDFA as R
+
+data ParsedValue a b = ParsedValue {
+  sourceValue :: a,
+  parsedValue :: b
+}
+instance Eq a => Eq (ParsedValue a b) where
+  x == y = sourceValue x == sourceValue y
+
+newtype CfgAud = CfgAud { unCfgAud :: ParsedValue (Maybe Text) R.Regex } deriving Eq
+
+parseCfgAud :: MonadFail m => Text -> m CfgAud
+parseCfgAud = fmap CfgAud . (fmap . ParsedValue . Just <*> parseRegex)
+  where
+    parseRegex = maybe (fail "jwt-aud should be a valid regular expression") pure . R.makeRegexM . bounded
+    -- need start and end of text bounds so that
+    -- regex does not match parts of text
+    bounded = ("\\`(" <>) . (<> "\\')")
+
+defaultCfgAud :: CfgAud
+defaultCfgAud = CfgAud $ ParsedValue Nothing $ R.makeRegex (".*"::Text)
 
 audMatchesCfg :: AppConfig -> Text -> Bool
-audMatchesCfg =  maybe (const True) (==) . configJwtAudience
+audMatchesCfg = R.matchTest . parsedValue . unCfgAud . configJwtAudience
 
 data AppConfig = AppConfig
   { configAppSettings              :: [(Text, Text)]
@@ -97,7 +121,7 @@ data AppConfig = AppConfig
   , configDbUri                    :: Text
   , configFilePath                 :: Maybe FilePath
   , configJWKS                     :: Maybe JwkSet
-  , configJwtAudience              :: Maybe Text
+  , configJwtAudience              :: CfgAud
   , configJwtRoleClaimKey          :: JSPath
   , configJwtSecret                :: Maybe BS.ByteString
   , configJwtSecretIsBase64        :: Bool
@@ -171,7 +195,7 @@ toText conf =
       ,("db-pre-config",             q . maybe mempty dumpQi . configDbPreConfig)
       ,("db-tx-end",                 q . showTxEnd)
       ,("db-uri",                    q . configDbUri)
-      ,("jwt-aud",                   q . fromMaybe mempty . configJwtAudience)
+      ,("jwt-aud",                   q . fold . sourceValue . unCfgAud . configJwtAudience)
       ,("jwt-role-claim-key",        q . T.intercalate mempty . fmap dumpJSPath . configJwtRoleClaimKey)
       ,("jwt-secret",                q . T.decodeUtf8 . showJwtSecret)
       ,("jwt-secret-is-base64",          T.toLower . show . configJwtSecretIsBase64)
@@ -279,7 +303,7 @@ parser optPath env dbSettings roleSettings roleIsolationLvl =
     <*> (fromMaybe "postgresql://" <$> optString "db-uri")
     <*> pure optPath
     <*> pure Nothing
-    <*> optStringOrURI "jwt-aud"
+    <*> (optStringEmptyable "jwt-aud" >>= maybe (pure defaultCfgAud) parseCfgAud)
     <*> parseRoleClaimKey "jwt-role-claim-key" "role-claim-key"
     <*> (fmap encodeUtf8 <$> optString "jwt-secret")
     <*> (fromMaybe False <$> optWithAlias
@@ -398,20 +422,6 @@ parser optPath env dbSettings roleSettings roleIsolationLvl =
 
     optStringEmptyable :: C.Key -> C.Parser C.Config (Maybe Text)
     optStringEmptyable k = overrideFromDbOrEnvironment C.optional k coerceText
-
-    optStringOrURI :: C.Key -> C.Parser C.Config (Maybe Text)
-    optStringOrURI k = do
-      stringOrURI <- mfilter (/= "") <$> overrideFromDbOrEnvironment C.optional k coerceText
-      -- If the string contains ':' then it should
-      -- be a valid URI according to RFC 3986
-      case stringOrURI of
-        Just s  -> if T.isInfixOf ":" s then validateURI s else return (Just s)
-        Nothing -> return Nothing
-      where
-        validateURI :: Text -> C.Parser C.Config (Maybe Text)
-        validateURI s = if isURI (T.unpack s)
-                          then return $ Just s
-                          else fail "jwt-aud should be a string or a valid URI"
 
     optInt :: (Read i, Integral i) => C.Key -> C.Parser C.Config (Maybe i)
     optInt k = join <$> overrideFromDbOrEnvironment C.optional k coerceInt
