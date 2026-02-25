7e9df6416195b3d9c534c1f1c4aeb39865869f45
Michał Kłeczek <michal@kleczek.org>
2026-02-25 19:36:48 +0100

    refactor: implement connection tracking in metrics
    Right now metrics observation handler does not track database connections but updates a single Gauge based on HasqlPoolObs events. This is problematic because Hasql pool reports various connection events in multiple phases. The connection state machine is not simple and to precisely report the number of connections in various states, it is necessary to track their lifecycles.
    
    This change adds a ConnTrack data structure and logic to track database connections lifecycles. At the moment it supports "connected" and "inUse" connection counts precisely. The "pgrst_db_pool_available" metric is implemented on top of ConnTrack instead of a simple Gauge.

--- test/spec/Feature/MetricsSpec.hs
+++ test/spec/Feature/MetricsSpec.hs
@@ -5,15 +5,18 @@
 
 module Feature.MetricsSpec where
 
-import           Network.Wai           (Application)
-import qualified PostgREST.AppState    as AppState
-import           PostgREST.Config      (AppConfig (configDbSchemas))
-import qualified PostgREST.Metrics     as Metrics
+import qualified Hasql.Pool.Observation as SQL
+import           Network.Wai            (Application)
+import qualified PostgREST.AppState     as AppState
+import           PostgREST.Config       (AppConfig (configDbSchemas))
+import           PostgREST.Metrics      (ConnStats (..),
+                                         connectionCounts)
+import qualified PostgREST.Metrics      as Metrics
 import           PostgREST.Observation
-import           Prometheus            (getCounter, getVectorWith)
+import           Prometheus             (getCounter, getVectorWith)
 import           Protolude
 import           SpecHelper
-import           Test.Hspec            (SpecWith, describe, it)
+import           Test.Hspec             (SpecWith, describe, it)
 
 spec :: SpecWith (((AppState.AppState, Metrics.MetricsState), Chan Observation), Application)
 spec = describe "Server started with metrics enabled" $ do
@@ -42,8 +45,34 @@ spec = describe "Server started with metrics enabled" $ do
         -- wait up to 2 secs so that retry can happen
         waitFor (2 * sec) "SchemaCacheLoadedObs" $ \x -> [ o | o@(SchemaCacheLoadedObs{}) <- pure x]
 
+  it "Should track in use connections" $ do
+    ((appState, metrics), waitFor) <- prepareState
+
+    -- we expect in use connections to be the same once finished
+    liftIO $ checkState' metrics
+        [
+            inUseConnections (+ 0)
+        ] $ do
+          signal <- newEmptyMVar
+          -- make sure waiting thread is signaled
+          bracket_ (pure ()) (putMVar signal ()) $
+            -- expecting one more connection in use
+            checkState' metrics
+            [
+                inUseConnections (+ 1)
+            ] $ do
+                -- start a thread hanging on a single connection until signaled
+                void $ forkIO $ void $ AppState.usePool appState $ liftIO (readMVar signal)
+                -- main thread waits for ConnectionObservation with InUseConnectionStatus
+                -- after which used connections count should be incremented
+                waitFor (1 * sec) "InUseConnectionStatus" $ \x -> [o | o@(HasqlPoolObs (SQL.ConnectionObservation _ SQL.InUseConnectionStatus)) <- pure x]
+
+          -- hanging thread was signaled and should return the connection
+          waitFor (1 * sec) "ReadyForUseConnectionStatus" $ \x -> [o | o@(HasqlPoolObs (SQL.ConnectionObservation _ SQL.ReadyForUseConnectionStatus)) <- pure x]
   where
     -- prometheus-client api to handle vectors is convoluted
     schemaCacheLoads label = expectField @"schemaCacheLoads" $
       (foldMap (Sum . round @Double @Int . snd) . find ((== label) . fst) <$>) . (`getVectorWith` getCounter)
+
+    inUseConnections = expectField @"connTrack" ((inUse <$>) . connectionCounts)
     sec = 1000000
