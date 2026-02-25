7785329c1643e0b771988b664266855155f9b3c2
Michał Kłeczek <michal@kleczek.org>
2026-02-25 19:36:48 +0100

    fix: implement connection tracking in metrics
    Right now metrics observation handler does not track database connections but updates a single Gauge based on HasqlPoolObs events. This is problematic because Hasql pool reports various connection events in multiple phases. The connection state machine is not simple and to precisely report the number of connections in various states, it is necessary to track their lifecycles.
    
    This change adds a ConnTrack data structure and logic to track database connections lifecycles. At the moment it supports "connected" and "inUse" connection counts precisely. The "pgrst_db_pool_available" metric is implemented on top of ConnTrack instead of a simple Gauge.

--- test/io/test_io.py
+++ test/io/test_io.py
@@ -2181,10 +2181,6 @@ def test_vary_default_header_set(defaultenv):
         assert response.headers["Vary"] == "Accept, Prefer, Range"
 
 
-@pytest.mark.xfail(
-    reason="pgrst_db_pool_available should not go negative on pg network failures",
-    strict=True,
-)
 def test_positive_pool_metric(defaultenv):
     "When a network failure is caused on the pg connection, pgrst_db_pool_available stays positive"
 
