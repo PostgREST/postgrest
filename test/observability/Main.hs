f6c371196d49e70fcf604bfac7d039a7f410483c
Michał Kłeczek <michal@kleczek.org>
2026-03-05 14:50:14 +0100

    test(refactor): Add Toxiproxy Hspec test infrastructure

--- test/observability/Main.hs
+++ test/observability/Main.hs
@@ -17,10 +17,13 @@ import           PostgREST.SchemaCache     (querySchemaCache)
 import qualified Observation.JwtCache
 import qualified Observation.MetricsSpec
 
+import qualified Data.Text                   as T
 import qualified Observation.SchemaCacheSpec
+import qualified Observation.ToxiSpec
 import           ObsHelper
 import           PostgREST.Observation       (Observation (HasqlPoolObs))
 import           Protolude                   hiding (toList, toS)
+import qualified System.Environment          as System
 import           Test.Hspec
 
 main :: IO ()
@@ -33,12 +36,16 @@ main = do
   -- this means we have another thread running for the entire duration of the spec but this shouldn't be a problem since Haskell green threads are lightweight
   void $ forkIO $ forever $ readChan poolChan
   metricsState <- Metrics.init (configDbPoolSize testCfg)
+  toxiProxyName <- T.pack <$> System.getEnv "TOXI_PROXY_NAME"
+  toxiPgPort <- T.pack <$> System.getEnv "TOXI_PGPORT"
+  pgPort <- T.pack <$> System.getEnv "PGPORT"
+  let toxiCfg = testCfg { configDbUri = "postgresql://localhost:" <> toxiPgPort }
   pool <- P.acquire $ P.settings
     [ P.size 3
     , P.acquisitionTimeout 10
     , P.agingTimeout 60
     , P.idlenessTimeout 60
-    , P.staticConnectionSettings (toUtf8 $ configDbUri testCfg)
+    , P.staticConnectionSettings (toUtf8 $ configDbUri toxiCfg)
     -- make sure metrics are updated and pool observations published to poolChan
     , P.observationHandler $ (writeChan poolChan <> Metrics.observationMetrics metricsState) . HasqlPoolObs
     ]
@@ -46,27 +53,30 @@ main = do
   actualPgVersion <- either (panic . show) id <$> P.use pool (queryPgVersion False)
 
   -- cached schema cache so most tests run fast
-  baseSchemaCache <- loadSCache pool testCfg
+  baseSchemaCache <- loadSCache pool toxiCfg
   loggerState <- Logger.init
 
   let
-    initApp sCache config = do
+    initApp sCache configure =
+      let config = configure toxiCfg in do
       -- duplicate poolChan as a starting point
       obsChan <- dupChan poolChan
       stateObsChan <- newObsChan obsChan
       appState <- AppState.initWithPool pool config loggerState metricsState (Metrics.observationMetrics metricsState <> writeChan obsChan)
       AppState.putPgVersion appState actualPgVersion
       AppState.putSchemaCache appState (Just sCache)
-      return (SpecState appState metricsState stateObsChan, postgrest (configLogLevel config) appState (pure ()))
+      return (SpecState appState metricsState stateObsChan $ testToxiProxy toxiProxyName toxiPgPort pgPort, postgrest (configLogLevel config) appState (pure ()))
 
   -- Run all test modules
   hspec $ do
     before (initApp baseSchemaCache testCfgJwtCache) $
       describe "Observation.JwtCacheObs" Observation.JwtCache.spec
-    before (initApp baseSchemaCache testCfg) $
-      describe "Feature.MetricsSpec" Observation.MetricsSpec.spec
-    before (initApp baseSchemaCache testCfg) $
-      describe "Feature.SchemaCacheSpec" Observation.SchemaCacheSpec.spec
+
+    traverse_ (before (initApp baseSchemaCache identity) . uncurry describe) [
+        ("Observation.MetricsSpec",     Observation.MetricsSpec.spec)
+      , ("Observation.SchemaCacheSpec", Observation.SchemaCacheSpec.spec)
+      , ("Observation.ToxiSpec",        Observation.ToxiSpec.spec)
+      ]
 
   where
     loadSCache pool conf =
