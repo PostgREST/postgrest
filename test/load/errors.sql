ab4dffe767bdce2c2bf6c84dccf7100ef2ea6028
Taimoor Zaeem <taimoorzaeem@gmail.com>
2026-04-25 12:16:07 +0500

    test(load): reduce num of tables in errors loadtest
    The table not found error only generates hint when the total number of
    tables in a schema are less than 500.
    
    Signed-off-by: Taimoor Zaeem <taimoorzaeem@gmail.com>

--- test/load/errors.sql
+++ test/load/errors.sql
@@ -1,7 +1,7 @@
 \ir fixtures.sql
 
 SELECT format('CREATE TABLE test.actors_%s ();', n)
-FROM generate_series(1, 20000) n
+FROM generate_series(1, 450) n -- 500 is the upper limit for table not found error hint generation
 \gexec
 
 -- TODO add many function for fuzzy search (somehow this is making the loadtest start slow)
