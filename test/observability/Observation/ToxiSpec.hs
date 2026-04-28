6b49acb43681b08114c01499a62bc72d34611464
Michał Kłeczek <michal@kleczek.org>
2026-04-28 12:49:26 +0200

    test: Add reproducer of 4622 (negative connection count)

--- test/observability/Observation/ToxiSpec.hs
+++ test/observability/Observation/ToxiSpec.hs
@@ -8,8 +8,12 @@ import           Control.Monad.Trans.Control (liftBaseDiscard)
 import           Network.Wai                 (Application)
 import           ObsHelper
 import qualified PostgREST.AppState          as AppState
+import           PostgREST.Metrics           (MetricsState (poolAvailable))
+import qualified PostgREST.Metrics           as Metric
+import           Prometheus                  (getGauge)
 import           Protolude                   hiding (get)
 import           Test.Hspec                  (SpecWith, describe, it)
+import           Test.Hspec.Expectations
 import           Test.Hspec.Wai
 import           Toxiproxy                   (withDisabled)
 
@@ -32,3 +36,18 @@ spec = describe "Tests using Toxiproxy" $ do
     liftBaseDiscard (withDisabled specToxiProxy) $ do
         void $ get "/items?id=eq.5"
             `shouldRespondWith` 503
+
+  it "Must not have negative connection count" $ do
+    SpecState{specAppState, specToxiProxy, specMetrics=Metric.MetricsState{poolAvailable}} <- getState
+
+    -- make sure there are no open connections
+    liftIO $ AppState.flushPool specAppState
+
+    liftBaseDiscard (withDisabled specToxiProxy) $ do
+        replicateM_ 5 $ get "/authors_only?id=eq.5"
+            `shouldRespondWith` 503
+
+    -- TODO https://github.com/PostgREST/postgrest/issues/4622
+    -- change to 0 when fixed
+    -- HSpec does not support xfail which should be used instead
+    liftIO $ getGauge poolAvailable >>= (`shouldBe` (-5::Int)) . round
