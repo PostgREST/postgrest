3588e5ce8385ff49c2b6e81309e87a007766ec2d
Steve Chavez <stevechavezast@gmail.com>
2026-04-30 16:46:26 -0500

    chore: Update src/PostgREST/Logger.hs

--- src/PostgREST/Logger.hs
+++ src/PostgREST/Logger.hs
@@ -189,7 +189,7 @@ observationMessages = \case
   DBListenRetry delay ->
     pure $ "Retrying listening for database notifications in " <> (show delay::Text) <> " seconds..."
   DBListenBugCallQueryFix ->
-    pure "This is likely a bug in the notification queue, executing the following to solve it: SELECT pg_notification_queue_usage();"
+    pure "This is likely a PostgreSQL bug in the notification queue, executing the following to try to solve it: SELECT pg_notification_queue_usage();"
   DBListenerGotSCacheMsg channel ->
     pure $ "Received a schema cache reload message on the " <> show channel <> " channel"
   DBListenerGotConfigMsg channel ->
