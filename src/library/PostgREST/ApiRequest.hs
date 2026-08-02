f34ca15e84c9bd538abd0ae3456877916c00052a
steve-chavez <stevechavezast@gmail.com>
2026-08-02 16:31:31 -0500

    add: configs as json GUC for db-root-spec
    These are needed for a complete OpenAPI spec

--- src/library/PostgREST/ApiRequest.hs
+++ src/library/PostgREST/ApiRequest.hs
@@ -57,6 +57,7 @@ import Protolude
 -}
 data ApiRequest = ApiRequest {
     iAction              :: Action                           -- ^ Action on the resource
+  , iIsRootRoutine       :: Bool                             -- ^ If the request wants the root routine
   , iRange               :: HM.HashMap Text NonnegRange      -- ^ Requested range of rows within response
   , iTopLevelRange       :: NonnegRange                      -- ^ Requested range of rows from the top level
   , iPayload             :: Maybe Payload                    -- ^ Data sent by client and used for mutation actions
@@ -76,7 +77,7 @@ data ApiRequest = ApiRequest {
 -- | Examines HTTP request and translates it into user intent.
 userApiRequest :: AppConfig -> Preferences.Preferences -> Request -> RequestBody -> Either ApiRequestError ApiRequest
 userApiRequest conf prefs req reqBody = do
-  resource <- getResource conf $ pathInfo req
+  (resource, isRootRoutine) <- getResource conf $ pathInfo req
   (schema, negotiatedByProfile) <- getSchema conf hdrs method
   act <- getAction resource schema method
   qPrms <- first QueryParamError $ QueryParams.parse (actIsInvokeSafe act) $ rawQueryString req
@@ -94,6 +95,7 @@ userApiRequest conf prefs req reqBody = do
   , iCookies = iCkies
   , iPath = rawPathInfo req
   , iMethod = method
+  , iIsRootRoutine = isRootRoutine
   , iSchema = schema
   , iNegotiatedByProfile = negotiatedByProfile
   , iAcceptMediaType = maybe [MTAny] (map MediaType.decodeMediaType . parseHttpAccept) $ lookupHeader "accept"
@@ -116,16 +118,16 @@ userPreferences conf req timezones = Preferences.fromHeaders (configDbTxAllowOve
 userBearerAuth :: Request -> Maybe ByteString
 userBearerAuth req = extractBearerAuth =<< lookup hAuthorization (requestHeaders req)
 
-getResource :: AppConfig -> [Text] -> Either ApiRequestError Resource
+getResource :: AppConfig -> [Text] -> Either ApiRequestError (Resource, Bool)
 getResource AppConfig{configOpenApiMode, configDbRootSpec} = \case
   []             ->
       case (configOpenApiMode,configDbRootSpec) of
         (OADisabled,_) -> Left OpenAPIDisabled
-        (_, Just qi)   -> Right $ ResourceRoutine (qiName qi)
-        (_, Nothing)   -> Right ResourceSchema
+        (_, Just qi)   -> Right (ResourceRoutine (qiName qi), True)
+        (_, Nothing)   -> Right (ResourceSchema, False)
 
-  [table]        -> Right $ ResourceRelation table
-  ["rpc", pName] -> Right $ ResourceRoutine pName
+  [table]        -> Right (ResourceRelation table, False)
+  ["rpc", pName] -> Right (ResourceRoutine pName , False)
   _              -> Left InvalidResourcePath
 
 getAction :: Resource -> Schema -> ByteString -> Either ApiRequestError Action
