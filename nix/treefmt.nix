5ffb5f186575d936b9eee07fd14833fee09b5fe7
Wolfgang Walther <walther@technowledgy.de>
2026-08-30 14:28:31 +0200

    nix: move hlint to treefmt
    -j is passed automatically via treefmt-nix, so removing that from the
    config file.
    
    (cherry picked from commit 720b26e93da59b080037906e8ab325b1477fa28e)

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -1,3 +1,19 @@
+{ pkgs, ... }:
+let
+  hlintConfig = pkgs.writeText "hlintConfig.yml" ''
+
+    # Arguments passed to hlint
+    - arguments: [-XQuasiQuotes, -XNoPatternSynonyms]
+
+    # Warnings
+    - warn: { lhs: "a == a", rhs: "True",  note: "This comparison always evaluates to True" }
+    - warn: { lhs: "a /= a", rhs: "False", note: "This comparison always evaluates to False" }
+    - warn: { lhs: "a < a",  rhs: "False", note: "This comparison always evaluates to False" }
+    - warn: { lhs: "a > a",  rhs: "False", note: "This comparison always evaluates to False" }
+    - warn: { lhs: "a <= a", rhs: "True",  note: "This comparison always evaluates to True" }
+    - warn: { lhs: "a >= a", rhs: "True",  note: "This comparison always evaluates to True" }
+  '';
+in
 {
   settings.verbose = 1;
   settings.on-unmatched = "debug";
@@ -5,8 +21,13 @@
   programs.actionlint.enable = true;
   programs.black.enable = true;
   programs.deadnix.enable = true;
+  programs.hlint.enable = true;
   programs.nixpkgs-fmt.enable = true;
   programs.ruff-check.enable = true;
   programs.statix.enable = true;
   programs.stylish-haskell.enable = true;
+
+  settings.formatter.hlint.options = [
+    "--hint=${hlintConfig}"
+  ];
 }
