729725153a572836274155527f06786225123706
Wolfgang Walther <walther@technowledgy.de>
2026-08-30 14:12:46 +0200

    nix: move black to treefmt
    (cherry picked from commit bbc21f38f4864515cc78bf1f34fb50320ee17cd8)

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -3,6 +3,7 @@
   settings.on-unmatched = "debug";
 
   programs.actionlint.enable = true;
+  programs.black.enable = true;
   programs.deadnix.enable = true;
   programs.nixpkgs-fmt.enable = true;
   programs.ruff-check.enable = true;
