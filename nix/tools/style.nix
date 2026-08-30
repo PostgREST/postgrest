5e1b78a4f46228778918b5b04d9abcdece95ecac
Wolfgang Walther <walther@technowledgy.de>
2026-08-30 14:28:31 +0200

    nix: move hlint to treefmt
    -j is passed automatically via treefmt-nix, so removing that from the
    config file.

--- nix/tools/style.nix
+++ nix/tools/style.nix
@@ -1,25 +1,11 @@
 { buildToolbox
 , checkedShellScript
 , fd
-, hlint
 , hsie
 , python3Packages
-, writeText
+
 }:
 let
-  hlintConfig = writeText "hlintConfig.yml" ''
-
-    # Arguments passed to hlint
-    - arguments: [-j, -XQuasiQuotes, -XNoPatternSynonyms]
-
-    # Warnings
-    - warn: { lhs: "a == a", rhs: "True",  note: "This comparison always evaluates to True" }
-    - warn: { lhs: "a /= a", rhs: "False", note: "This comparison always evaluates to False" }
-    - warn: { lhs: "a < a",  rhs: "False", note: "This comparison always evaluates to False" }
-    - warn: { lhs: "a > a",  rhs: "False", note: "This comparison always evaluates to False" }
-    - warn: { lhs: "a <= a", rhs: "True",  note: "This comparison always evaluates to True" }
-    - warn: { lhs: "a >= a", rhs: "True",  note: "This comparison always evaluates to True" }
-  '';
 
   lint =
     checkedShellScript
@@ -36,10 +22,6 @@ let
 
         echo "Checking consistency of import aliases in Haskell code..."
         ${hsie} check-aliases src/library src/executable
-
-        echo "Linting Haskell files..."
-        ${fd}/bin/fd '\.l?hs$' \
-          | xargs ${hlint}/bin/hlint --hint=${hlintConfig}
       '';
 
 in
